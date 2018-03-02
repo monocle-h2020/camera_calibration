9e543ea18debf4f13dc8e35dc4ab023b1da50758
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-03-02 17:22:47 +0100

    Peak fitting

--- wavelength.py
+++ wavelength.py
@@ -4,8 +4,7 @@ import rawpy
 from cv2 import resize
 from scipy.ndimage.filters import gaussian_filter1d as gauss
 from sys import argv
-import subprocess
-import os
+from scipy.optimize import curve_fit
 
 filename = argv[1]
 
@@ -47,7 +46,37 @@ for D in (thickF, thinF):
 for D, DF in zip([thick, thin], [thickF, thinF]):
     for d in (D, DF):
         for j in (0,1,2):
-            plt.plot(d[:, 175, j], c=RGB[j])
+            plt.plot(d[:, 100, j], c=RGB[j])
         plt.xlim(0, d.shape[0])
         plt.ylim(0,255)
         plt.show()
+
+def find_3peaks(D):
+    return D.argmax(axis=0)
+
+for DF in [thickF, thinF]:
+    peaks = find_3peaks(DF)
+    for j in (0,1,2):
+        plt.plot(peaks[:, j], c=RGB[j])
+    plt.xlim(0, DF.shape[1])
+    plt.ylim(0, DF.shape[0])
+    plt.show()
+
+def wavelength(coords, a, b, c, d):
+    x, y = coords
+    return a*x + b*y**2 + c*y + d
+
+peaks_thick = find_3peaks(thickF)
+peaks_thin = find_3peaks(thinF)
+y = np.concatenate((np.arange(col0, col1), np.arange(col2, col3)))
+p = np.concatenate((peaks_thick, peaks_thin))
+p_fit = p.copy()
+for j in (0,1,2):
+    coeff = np.polyfit(y, p[:,j], 2)
+    p_fit[:,j] = np.polyval(coeff, y)
+
+coeffarr = np.tile(np.nan, (y.shape[0], 2))
+wvlfit = np.tile(np.nan, (y.shape[0], 3))
+for i, col in enumerate(y):
+    coeffarr[i] = np.polyfit(p_fit[i], TLpeaks[[3,2,0]], 1)
+    wvlfit[i] = np.polyval(coeffarr[i], p_fit[i])
