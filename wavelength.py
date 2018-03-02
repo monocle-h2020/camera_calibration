a524ce5ad18b679d18e7a5c6cd5d1467135991f6
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-03-02 12:14:38 +0100

    Test Gaussian convolution

--- wavelength.py
+++ wavelength.py
@@ -4,13 +4,18 @@ import rawpy
 from cv2 import resize
 from scipy.ndimage.filters import gaussian_filter1d as gauss
 from sys import argv
+import subprocess
+import os
 
 filename = argv[1]
 
+RGB = ["r", "g", "b"]
 TLpeaks = np.array([436.6, 487.7, 544.45, 611.6])
 
-col0 = 1512
-col1 = 2315
+col0 = 1501
+col1 = 1911
+col2 = 1949
+col3 = 2315
 
 row0 = 2300
 row1 = 3750
@@ -19,16 +24,34 @@ img = rawpy.imread(filename)
 data = img.postprocess(use_camera_wb=True, gamma=(1,1), output_bps=8)
 #data_res = resize(data, (data.shape[1]//n, data.shape[0]//n))
 
-crop = data[row0:row1, col0:col1]
-plt.imshow(crop)
+thick = data[row0:row1, col0:col1]
+thin  = data[row0:row1, col2:col3]
+
+for D in (thick, thin):
+    plt.imshow(D)
+    plt.show()
+
+def gauss_filter(D, sigma=5, *args, **kwargs):
+    """
+    Apply a 1-D Gaussian kernel along the wavelength axis
+    """
+    return gauss(D.astype(float), sigma, *args, axis=0, **kwargs)
+
+thickF = gauss_filter(thick)
+thinF  = gauss_filter(thin )
+
+for D in (thickF, thinF):
+    plt.imshow(D)
+    plt.show()
+
+for j in (0,1,2):
+    plt.plot(thick[:, 175, j], c=RGB[j])
+plt.xlim(0, thick.shape[0])
+plt.ylim(0,255)
 plt.show()
 
-filtered = gauss(crop.astype(float), 5, axis=0)
-
-for x in (200, 600):
-    RGB = ["r", "g", "b"]
-    for j in (0,1,2):
-        plt.plot(filtered[:,x,j], c=RGB[j])
-    plt.xlim(0, crop.shape[0])
-    plt.ylim(0, 255)
-    plt.show()
+for j in (0,1,2):
+    plt.plot(thickF[:, 175, j], c=RGB[j])
+plt.xlim(0, thickF.shape[0])
+plt.ylim(0,255)
+plt.show()
