b4e29ab1e51102768a4f7bcdb452a5e606960a96
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-05-17 13:34:00 +0200

    RAWer RAWs

--- ispex/io.py
+++ ispex/io.py
@@ -1,8 +1,12 @@
 import rawpy
 import exifread
 
-def load_dng(filename, gamma=(1,1), output_bps=8, **kwargs):
+def load_dng_raw(filename):
     img = rawpy.imread(filename)
+    return img
+
+def load_dng(filename, gamma=(1,1), output_bps=8, **kwargs):
+    img = load_dng_raw(filename)
     data = img.postprocess(gamma=gamma, output_bps=output_bps, **kwargs)
     return data
 
