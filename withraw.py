3d1e4b06e8b8413704eb9dacb4d07e3e14886560
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-05-24 17:26:29 +0200

    Wavelength calibration in withraw

--- withraw.py
+++ withraw.py
@@ -3,26 +3,31 @@ import rawpy
 from sys import argv
 from matplotlib import pyplot as plt, patheffects as pe
 from ispex.general import gauss_filter
-from ispex import raw
-from ispex import plot, io
+from ispex import raw, plot, io, wavelength
 
 filename = argv[1]
 handle = filename.split()
 
 img = io.load_dng_raw(filename)
-imgarray = img.raw_image.astype(np.int16)
-RGBG, offsets = raw.pull_apart(imgarray, img.raw_pattern)
+image_cut  = raw.cut_out_spectrum(img.raw_image)
+colors_cut = raw.cut_out_spectrum(img.raw_colors)
 
-plot.RGBG_stacked(RGBG, saveto="RGBG_stacked.png")
+RGBG, offsets = raw.pull_apart(image_cut, colors_cut)
+plot.RGBG_stacked(RGBG, extent=(raw.xmin, raw.xmax, raw.ymax, raw.ymin), show_axes=True, saveto="RGBG_cutout.png")
 
-cutout = raw.cut_out_spectrum(RGBG)
-plot.RGBG_stacked(cutout, saveto="RGBG_stacked_cutout.png", boost=5)
+coefficients = wavelength.load_coefficients("wavelength_solution.npy")
+wavelengths_cut = wavelength.calculate_wavelengths(coefficients, raw.x, raw.y)
+wavelengths_split, offsets = raw.pull_apart(wavelengths_cut, colors_cut)
 
-plot.plot_spectrum(raw.x, cutout.mean(0), xlabel="Pixel", ylabel="Mean RGBG value")
-
-plot.RGBG_stacked_with_graph(cutout, saveto="RGBG_spectrum.png")
+lambdarange, all_interpolated = wavelength.interpolate_multi(wavelengths_split, RGBG)
+plot.RGBG_stacked(all_interpolated, extent=(lambdarange[0], lambdarange[-1], raw.ymax, raw.ymin), show_axes=True, xlabel="$\lambda$ (nm)", aspect=0.5 * len(lambdarange) / len(raw.x), saveto="RGBG_cutout_corrected.png")
+plot.RGBG_stacked_with_graph(all_interpolated, x=lambdarange, extent=(lambdarange[0], lambdarange[-1], raw.ymax, raw.ymin), xlabel="$\lambda$ (nm)", aspect=0.5 * len(lambdarange) / len(raw.x), saveto="RGBG_cutout_corrected_spectrum.png")
 
 plot.RGBG(RGBG, vmax=800, saveto="RGBG_split.png", size=30)
-RGBGg = gauss_filter(RGBG)
-Rg, Gg, Bg, G2g = raw.split_RGBG(RGBGg)
-plot.RGBG(RGBGg, vmax=800, saveto="RGBG_split_gauss.png", size=30)
+plot.RGBG(all_interpolated, vmax=800, saveto="RGBG_split_interpolated.png", size=30)
+
+stacked = wavelength.stack(lambdarange, all_interpolated)
+
+sub = img.raw_image[1750:, 1750:]
+plt.hist(sub.ravel(), bins=20)
+plt.show()
