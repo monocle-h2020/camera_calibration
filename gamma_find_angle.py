2f1247bbedc6cd5d821b8e7d6b2a5f09ad0ddc7f
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-05-30 11:33:18 +0200

    RGB have different angles

--- gamma_find_angle.py
+++ gamma_find_angle.py
@@ -2,7 +2,8 @@ import numpy as np
 import rawpy
 from sys import argv
 from matplotlib import pyplot as plt, patheffects as pe
-from ispex.general import gauss_filter
+from ispex.general import cut
+from ispex.gamma import cos4f, find_I0
 from ispex import raw, plot, io, wavelength
 from scipy.optimize import curve_fit
 from glob import glob
@@ -10,29 +11,17 @@ from glob import glob
 folder = argv[1]
 files = glob(folder+"/*.dng")
 
-def cut(arr, x=250, y=250):
-    return arr[y:-y, x:-x]
-
-def cos4(d, p, a):
-    return a*(np.cos(d/p))**4
-
-def cos4f(d, f, a):
-    return a*(np.cos(np.arctan(d/f)))**4
-
-def find_I0(rgbg, distances, radius=100):
-    return rgbg[distances < radius].mean()
-
-p0s = []
-v0s = []
+p0s = [[], [], []]
+v0s = [[], [], []]
 
 for filename in files:
+    print(filename)
     img = io.load_dng_raw(filename)
 
     image_cut  = cut(img.raw_image)
     colors_cut = cut(img.raw_colors)
 
     RGBG, offsets = raw.pull_apart(image_cut, colors_cut)
-    #plot.RGBG_stacked(RGBG, show_axes=True, boost=1)
 
     x = np.arange(image_cut.shape[1])
     y = np.arange(image_cut.shape[0])
@@ -41,16 +30,30 @@ for filename in files:
     D = np.sqrt((X - x0)**2 + (Y - y0)**2)
     D_split, offsets = raw.pull_apart(D, colors_cut)
 
-    RGBG_norm = RGBG.astype(np.float) - 528
+    RGBG_norm = RGBG.astype(np.float)
 
     for j in range(4):
-        popt, pcov = curve_fit(cos4f, D_split[..., j].ravel(), RGBG_norm[..., j].ravel(), p0=[3000, 1000])
+        C = RGBG_norm[..., j]
+        if len(np.where(C > 4000)[0]):
+            continue
+        D = D_split[..., j]
+        I0 = find_I0(C, D)
+        popt, pcov = curve_fit(cos4f, D.ravel(), C.ravel(), p0=[I0, 1000, 531])
         print(popt, np.sqrt(np.diag(pcov)))
-        p0s.append(popt[0])
-        v0s.append(pcov[0,0])
+        col = j if j <= 2 else 1
+        p0s[col].append(popt[0])
+        v0s[col].append(pcov[0,0])
+    print("----")
 
-p0s = np.array(p0s)
-v0s = np.array(v0s)
+means = np.empty(3)
+for j, p, v in zip(range(3), p0s, v0s):
+    p = np.array(p)
+    v = np.array(v)
+    mean = np.average(p, weights=1/v)
+    print(mean)
+    print(p.astype(int))
+    print(v.astype(int))
+    print("---")
+    means[j] = mean
 
-mean = np.average(p0s, weights=1/v0s)
-np.save("pixel_angle.npy", mean)
+np.save("pixel_angle.npy", means)
