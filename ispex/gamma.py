07c64e4f69d9f582e2a72a4ef31bcb82cbb9b914
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-06-12 12:49:34 +0200

    Malus law error

--- ispex/gamma.py
+++ ispex/gamma.py
@@ -1,11 +1,21 @@
 import numpy as np
 
+degrad = np.pi/180
 polariser_angle = 74.14017
 pixel_angle = np.array([2825.46078149, 3205.27866603, 3120.66102908])
 I_range = np.linspace(0, 1, 501)
 
-def malus(angle):
-    return (np.cos(np.radians(angle)))**2
+def malus(angle, offset=polariser_angle):
+    return (np.cos(np.radians(angle-offset)))**2
+
+def malus_error(angle0, angle1=polariser_angle, I0=1., sigma_angle0=2., sigma_angle1=0.5, sigma_I0=0.01):
+    alpha = angle0 - angle1
+    A = I0 * degrad * np.sin(np.pi/90 * (alpha))
+    s_a2 = A**2 * (sigma_angle0**2 + sigma_angle1**2)
+    s_I2 = (malus(angle0, offset=angle1) * sigma_I0)**2
+    total = np.sqrt(s_I2 + s_a2)
+
+    return total
 
 def cos4f(d, f, a, o):
     return o + a*(np.cos(np.arctan(d/f)))**4
