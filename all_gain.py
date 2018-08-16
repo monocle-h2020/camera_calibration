fc2ec67fdca277c380b041228dc30132f4bb1af3
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-08-16 10:13:00 +0200

    Flatfielded gain method

---
+++ all_gain.py
@@ -0,0 +1,72 @@
+import numpy as np
+from sys import argv
+from matplotlib import pyplot as plt
+from ispex import raw, plot, io
+from ispex.general import bin_centers, weighted_mean
+from ispex.gamma import malus, malus_error
+from glob import glob
+from scipy.optimize import curve_fit
+
+folder_main = argv[1]
+folders = glob(folder_main+"/*iso*")
+isos = np.tile(np.nan, len(folders))
+gains = isos.copy()
+gainerrs = gains.copy()
+RONs = gains.copy()
+RONerrs = RONs.copy()
+
+for i,folder in enumerate(folders):
+    isos[i] = int(folder.split("iso")[-1])
+    try:
+        gains[i], gainerrs[i], RONs[i], RONerrs[i] = np.load(folder+"/gain_ron.npy")
+    except FileNotFoundError:
+        continue
+
+invgains = 1/gains
+invgainerrs = invgains**2 * gainerrs
+
+def model(iso, slope, offset, knee):
+    iso2 = np.copy(iso)
+    results = np.tile(knee * slope + offset, len(iso2))
+    results[iso2 < knee] = iso2[iso2 < knee] * slope + offset
+    return results
+
+def model_err(iso, popt, pcov):
+    iso2 = np.copy(iso)
+    results = np.tile(popt[2]**2 * pcov[0,0] + popt[0]**2 * pcov[2,2] + pcov[1,1], len(iso2))
+    results[iso2 < popt[2]] = iso2[iso2 < popt[2]]**2 * pcov[0,0] + pcov[1,1]
+    results = np.sqrt(results)
+    return results
+
+ind = np.where(~np.isnan(gains))
+popt, pcov = curve_fit(model, isos[ind], invgains[ind], p0=[0.1, 0.1, 200], sigma=invgainerrs[ind])
+
+irange = np.arange(0, 1850, 3)
+invgain_fit = model(irange, *popt)
+err_fit = model_err(irange, popt, pcov)
+
+fit_measured = model(isos, *popt)
+SS_res = np.sum((invgains[ind] - fit_measured[ind])**2)
+SS_tot = np.sum((invgains[ind] - invgains[ind].mean())**2)
+R2 = 1 - SS_res/SS_tot
+
+for xmax in (1850, 250):
+    plt.errorbar(isos, invgains, yerr=invgainerrs, fmt="o", c="k")
+    plt.plot(irange, invgain_fit, c="k", label=f"slope: {popt[0]:.4f}\noffset: {popt[1]:.4f}\nknee: {popt[2]:.0f}")
+    plt.fill_between(irange, invgain_fit-err_fit, invgain_fit+err_fit, color="0.5",
+                     label=f"$\sigma$ slope: {np.sqrt(pcov[0,0]):.4f}\n$\sigma$ offset: {np.sqrt(pcov[1,1]):.4f}\n$\sigma$ knee: {np.sqrt(pcov[2,2]):.0f}")
+    plt.xlabel("ISO")
+    plt.ylabel("$1/G$ (ADU/e$^-$)")
+    plt.xlim(0, xmax)
+    plt.ylim(0, 5)
+    plt.title(f"$R^2 = {R2:.3f}$")
+    plt.legend(loc="lower right")
+    plt.show()
+    plt.close()
+
+    plt.errorbar(isos, RONs, yerr=RONerrs, fmt="o", c="k")
+    plt.xlabel("ISO")
+    plt.ylabel("RON (e$^-$)")
+    plt.xlim(0, xmax)
+    plt.show()
+    plt.close()
---
+++ gain_ron.py
@@ -0,0 +1,75 @@
+import numpy as np
+from sys import argv
+from matplotlib import pyplot as plt
+from ispex import raw, plot, io
+from ispex.general import bin_centers, weighted_mean
+from ispex.gamma import malus, malus_error
+from glob import glob
+from scipy.stats import binned_statistic
+
+folder_main = argv[1]
+iso = int(folder_main.split("iso")[-1])
+folders_pol = glob(folder_main+"/*")
+folders_pol = [f for f in folders_pol if "." not in f]
+Vs = np.zeros_like(folders_pol, dtype=np.float32)
+Verrs = Vs.copy()
+Ms = Vs.copy()
+Merrs = Ms.copy()
+for i,folder in enumerate(folders_pol):
+    files = glob(folder+"/*.dng")
+
+    arrs, colors = io.load_dng_many(f"{folder}/*.dng", return_colors=True)
+
+    mean = arrs.mean(axis=0).astype(np.float32)  # mean per x,y
+
+    means = arrs.mean(axis=(1,2)) - 528
+    mean_all = means.mean()
+    mean_err = means.std() / np.sqrt(len(means) - 1)
+
+    new_arrs = arrs - mean
+
+    stds = new_arrs.std(axis=(1,2))
+    std_mean = stds.mean()
+    var = std_mean**2
+    var_err = stds.std() / np.sqrt(len(stds) - 1) * 2 * std_mean
+    Vs[i], Verrs[i], Ms[i], Merrs[i] = var, var_err, mean_all, mean_err
+
+    print(f"{(i+1)/len(folders_pol)*100:.0f}%", end=" ")
+
+print("")
+
+fit_min = 0
+fit_max = 2000
+ind = np.where((Ms > fit_min) & (Ms < fit_max))
+try:
+    fit, cov = np.polyfit(Ms[ind], Vs[ind], 1, w=1/Verrs[ind], cov=True)
+except ValueError:
+    print("NO COVARIANCE MATRIX")
+    fit = np.polyfit(Ms[ind], Vs[ind], 1)
+    cov = np.tile(np.nan, (2,2))
+fiterr = np.sqrt(np.diag(cov))
+gain = 1/fit[0]
+gainerr = gain**2 * fiterr[0]
+RON  = gain * np.sqrt(fit[1])
+RONerr = np.sqrt(gainerr**2 * fit[1] + 0.25 * fiterr[1]**2 * gain**2 / fit[1])
+
+plt.figure(figsize=(7,5), tight_layout=True)
+x = np.logspace(-1, 4, 500)
+y = np.polyval(fit, x)
+yerr = np.sqrt(fiterr[0]**2 * x**2 + fiterr[1]**2)
+plt.axvline(fit_min, ls="--", c="0.5")
+plt.axvline(fit_max, ls="--", c="0.5")
+plt.plot(x, y, c='k', label=f"$G = {gain:.3f}$ e$^-$/ADU")
+plt.axhline(fit[1], c='k', ls="--", label=f"$RON = {RON:.3f}$ e$^-$")
+plt.fill_between(x, y-yerr, y+yerr, color="0.5", label=f"$\sigma_G = {gainerr:.3f}$\n$\sigma_R = {RONerr:.2}$")
+plt.errorbar(Ms, Vs, xerr=Merrs, yerr=Verrs, fmt='o', c='k', label=f"$ISO = {iso}$")
+plt.xscale("log") ; plt.yscale("log")
+plt.xlim(0.1, 4096) ; plt.ylim(ymin=0.9*fit[1])
+plt.xlabel("Mean (ADU)")
+plt.ylabel("Variance (ADU$^2$)")
+plt.legend(loc="upper left")
+plt.savefig(f"results/gain_new/G_RON_iso{iso}.png")
+plt.show()
+
+results = np.array([gain, gainerr, RON, RONerr])
+np.save(f"{folder_main}/gain_ron.npy", results)
