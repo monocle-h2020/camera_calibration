8e5e6766cb35c6b79e02fc4555d69634601fbae4
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-08-30 12:08:16 +0200

    Gain plot and LUT generation

--- all_gain.py
+++ all_gain.py
@@ -44,12 +44,22 @@ popt, pcov = curve_fit(model, isos[ind], invgains[ind], p0=[0.1, 0.1, 200], sigm
 irange = np.arange(0, 1850, 3)
 invgain_fit = model(irange, *popt)
 err_fit = model_err(irange, popt, pcov)
+gain_fit = 1/invgain_fit
+gain_err_fit = err_fit / invgain_fit**2
 
 fit_measured = model(isos, *popt)
 R2 = Rsquare(invgains[ind], fit_measured[ind])
 
 invRONs = invgains * RONs
 invRONerrs = np.sqrt(invgains**2 * RONerrs**2 + RONs**2 * invgainerrs**2)
+
+LUT_iso = np.arange(0, 1840, 1)
+LUT_invgain = model(LUT_iso, *popt)
+LUT_invgain_err = model_err(LUT_iso, popt, pcov)
+LUT_gain = 1/LUT_invgain
+LUT_gain_err = LUT_invgain_err
+LUT = np.stack([LUT_iso, LUT_gain, LUT_gain_err])
+np.save("results/gain_new/LUT.npy", LUT)
 
 for xmax in (1850, 250):
     plt.figure(figsize=(7,5), tight_layout=True)
@@ -63,6 +73,21 @@ for xmax in (1850, 250):
     plt.ylim(0, 5)
     plt.title(f"$R^2 = {R2:.4f}$")
     plt.legend(loc="lower right")
+    plt.savefig(f"results/gain_new/iso_invgain_{xmax}.png")
+    plt.show()
+    plt.close()
+
+    plt.figure(figsize=(7,5), tight_layout=True)
+    plt.errorbar(isos, gains, yerr=gainerrs, fmt="o", c="k")
+    plt.plot(irange, gain_fit, c="k", label=f"slope: {popt[0]:.4f}\noffset: {popt[1]:.4f}\nknee: {popt[2]:.1f}")
+    plt.fill_between(irange, gain_fit-gain_err_fit, gain_fit+gain_err_fit, color="0.5",
+                     label=f"$\sigma$ slope: {np.sqrt(pcov[0,0]):.4f}\n$\sigma$ offset: {np.sqrt(pcov[1,1]):.4f}\n$\sigma$ knee: {np.sqrt(pcov[2,2]):.1f}")
+    plt.xlabel("ISO")
+    plt.ylabel("$G$ (e$^-$/ADU)")
+    plt.xlim(0, xmax)
+    plt.ylim(0, 5)
+    plt.title(f"$R^2 = {R2:.4f}$")
+    plt.legend(loc="upper right")
     plt.savefig(f"results/gain_new/iso_gain_{xmax}.png")
     plt.show()
     plt.close()
