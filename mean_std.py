99d4ad9b86f19cdf0fabb0192f980adaba38fd33
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-09-24 16:41:45 +0200

    More general

--- mean_std.py
+++ mean_std.py
@@ -5,19 +5,19 @@ from glob import glob
 from os import walk
 
 folder = argv[1]
-subfolders = next(walk(folder))[1]
-folders = [folder] + [f"{folder}/{sub}" for sub in subfolders]
 
-for fol in folders:
-    print(fol)
+for tup in walk(folder):
+    fol = tup[0]
+    name = fol.strip("/")
+
     DNGs = glob(f"{fol}/*.dng")
     if len(DNGs) == 0:
         continue
+
     arrs, colors = io.load_dng_many(fol+"/*.dng", return_colors=True)
     mean = arrs.mean(axis=0, dtype=np.float32)
     stds = arrs.std (axis=0, dtype=np.float32)
 
-    name = fol.strip("/")
     np.save(f"{name}_mean.npy", mean)
     np.save(f"{name}_stds.npy", stds)
     np.save(f"{name}_colr.npy", colors)
