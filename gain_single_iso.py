4948ef473aba69597376974a4a1fc1cc221ce735
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-10-07 16:09:53 +0200

    Calculate gain for single ISO

--- gain_single_iso.py
+++ gain_single_iso.py
@@ -8,75 +8,39 @@ from glob import glob
 folder = argv[1]
 iso = io.split_iso(folder)
 
-names, means = io.load_means(folder)
-names, stds  = io.load_stds (folder)
+names, means = io.load_means (folder)
+names, stds  = io.load_stds  (folder)
 colours      = io.load_colour(folder)
 
-raise Exception
-try:
-    iso = int(folder_main.split("iso")[-1].strip("/"))
-except:
-    iso = "_".join(folder_main.replace("\\", "/").split("/")).strip("_")
-folders_pol = glob(folder_main+"/*")
-folders_pol = [f for f in folders_pol if "." not in f]
-Vs = np.zeros_like(folders_pol, dtype=np.float32)
-Verrs = Vs.copy()
-Ms = Vs.copy()
-Merrs = Ms.copy()
-maxes = Ms.copy()
-VsRGBG = np.zeros((len(folders_pol), 4))
-VerrsRGBG = VsRGBG.copy()
-MsRGBG = VsRGBG.copy()
-MerrsRGBG = MsRGBG.copy()
-for i,folder in enumerate(folders_pol):
-    files = glob(folder+"/*.dng")
+means -= 528  # bias correction
 
-    arrs, colors = io.load_dng_many(f"{folder}/*.dng", return_colors=True)
-    arrs_RGBG = np.moveaxis(arrs, 0, 2)
-    arrs_RGBG, _ = raw.pull_apart(arrs_RGBG, colors)
-    arrs_RGBG = np.moveaxis(arrs_RGBG, 3, 1)
+mean_reshaped = np.moveaxis(means, 0, 2)
+stds_reshaped = np.moveaxis(stds , 0, 2)
 
-    max_here = arrs.max()
-    mean = arrs.mean(axis=0).astype(np.float32)  # mean per x,y
-    mean_RGBG, _ = raw.pull_apart(mean, colors)
+variances = stds_reshaped**2
+mean_RGBG, _ = raw.pull_apart(mean_reshaped, colours)
+vars_RGBG, _ = raw.pull_apart(variances    , colours)
 
-    means = arrs.mean(axis=(1,2)) - 528
-    means_RGBG = arrs_RGBG.mean(axis=(2,3)) - 528
-    mean_all = means.mean()
-    mean_all_RGBG = means_RGBG.mean(axis=1)
-    mean_err = means.std() / np.sqrt(len(means) - 1)
-    mean_err_RGBG = means_RGBG.std(axis=1) / np.sqrt(len(means) - 1)
+mean_mean = mean_RGBG.mean(axis=(1,2))
+vars_mean = vars_RGBG.mean(axis=(1,2))
+mean_stds = mean_RGBG.std (axis=(1,2))
+vars_stds = vars_RGBG.std (axis=(1,2))
 
-    new_arrs = arrs - mean
-    new_arrs_RGBG = arrs_RGBG - mean_RGBG[:,np.newaxis,...]
-
-    stds = new_arrs.std(axis=(1,2))
-    stds_RGBG = new_arrs_RGBG.std(axis=(2,3))
-    std_mean = stds.mean()
-    std_mean_RGBG = stds_RGBG.mean(axis=1)
-    var = std_mean**2
-    var_RGBG = std_mean_RGBG**2
-    var_err = stds.std() / np.sqrt(len(stds) - 1) * 2 * std_mean
-    var_err_RGBG = stds_RGBG.std(axis=1) / np.sqrt(stds_RGBG.shape[1] - 1) * 2 * std_mean_RGBG
-    Vs[i], Verrs[i], Ms[i], Merrs[i], maxes[i] = var, var_err, mean_all, mean_err, max_here
-    VsRGBG[i], VerrsRGBG[i], MsRGBG[i], MerrsRGBG[i] = var_RGBG, var_err_RGBG, mean_all_RGBG, mean_err_RGBG
-
-    print(f"{(i+1)/len(folders_pol)*100:.0f}%", end=" ")
-
-print("")
-
-Ms = MsRGBG.ravel()
-Vs = VsRGBG.ravel()
-Verrs = VerrsRGBG.ravel()
+mean_errors = mean_stds / np.sqrt(mean_RGBG.shape[1] * mean_RGBG.shape[2])
+vars_errors = vars_stds / np.sqrt(vars_RGBG.shape[1] * vars_RGBG.shape[2])
 
 fit_min = 0
 fit_max = 2000
-ind = np.where((Ms > fit_min) & (Ms < fit_max))
+ind = np.where((mean_mean > fit_min) & (mean_mean < fit_max))
+mean_fit        = mean_mean  [ind].ravel()
+vars_fit        = vars_mean  [ind].ravel()
+vars_errors_fit = vars_errors[ind].ravel()
+
 try:
-    fit, cov = np.polyfit(Ms[ind], Vs[ind], 1, w=1/Verrs[ind], cov=True)
+    fit, cov = np.polyfit(mean_fit, vars_fit, 1, w=1/vars_errors_fit, cov=True)
 except ValueError:
     print("NO COVARIANCE MATRIX")
-    fit = np.polyfit(Ms[ind], Vs[ind], 1)
+    fit = np.polyfit(mean_mean[ind], vars_mean[ind], 1)
     cov = np.tile(np.nan, (2,2))
 fiterr = np.sqrt(np.diag(cov))
 gain = 1/fit[0]
@@ -84,8 +48,8 @@ gainerr = gain**2 * fiterr[0]
 RON  = gain * np.sqrt(fit[1])
 RONerr = np.sqrt(gainerr**2 * fit[1] + 0.25 * fiterr[1]**2 * gain**2 / fit[1])
 
-fit_measured = np.polyval(fit, Ms)
-R2 = Rsquare(Vs[ind], fit_measured[ind])
+fit_measured = np.polyval(fit, mean_fit)
+R2 = Rsquare(vars_fit, fit_measured)
 
 plt.figure(figsize=(7,5), tight_layout=True)
 x = np.logspace(-1, 4, 500)
@@ -99,7 +63,7 @@ plt.fill_between(x, y-yerr, y+yerr, color="0.5", label=f"$\sigma_G = {gainerr:.3
 plt.errorbar([-1], [-1], xerr=[10], yerr=[10], fmt='o', c='k', label=f"$ISO = {iso}$")
 for j in range(4):
     c = "rgbg"[j]
-    plt.errorbar(MsRGBG[:,j], VsRGBG[:,j], xerr=MerrsRGBG[:,j], yerr=VerrsRGBG[:,j], fmt=f"{c}o")
+    plt.errorbar(mean_mean[j], vars_mean[j], xerr=mean_errors[j], yerr=vars_errors[j], fmt=f"{c}o")
 plt.xscale("log") ; plt.yscale("log")
 plt.xlim(0.1, 4096) ; plt.ylim(ymin=0.9*fit[1])
 plt.xlabel("Mean (ADU)")
@@ -110,4 +74,4 @@ plt.savefig(f"results/gain_new/G_RON_iso{iso}.png")
 plt.show()
 
 results = np.array([gain, gainerr, RON, RONerr])
-np.save(f"{folder_main}/gain_ron.npy", results)
+#np.save(f"{folder_main}/gain_ron.npy", results)
