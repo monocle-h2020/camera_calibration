8ea0b296835b78f61447798ab4d2a946397231eb
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-10-11 22:19:57 +0200

    Use pathlib

--- readnoise.py
+++ readnoise.py
@@ -4,12 +4,13 @@ from matplotlib import pyplot as plt
 from phonecal import raw, plot, io
 from phonecal.general import gaussMd
 
-folder = argv[1]
-isos, means = io.load_means (folder, retrieve_value=io.split_iso, file=True)
-isos, stds  = io.load_stds  (folder, retrieve_value=io.split_iso, file=True)
-colours     = io.load_colour(folder)
+folder = io.path_from_input(argv)
+root, images, stacks, products, results = io.folders(folder)
+isos, means = io.load_means (folder, retrieve_value=io.split_iso)
+isos, stds  = io.load_stds  (folder, retrieve_value=io.split_iso)
+colours     = io.load_colour(stacks)
 
-gain_table = np.load(folder.replace("stacks", "products").replace("bias", "gain").strip("/")+"_lookup_table.npy")
+gain_table = np.load(products/"gain_lookup_table.npy")
 
 low_iso = isos.argmin()
 high_iso= isos.argmax()
@@ -26,7 +27,7 @@ for ind in (high_iso, low_iso):
     plt.ylabel("Frequency")
     plt.ylim(0.9, std.size)
     plt.grid(ls="--")
-    plt.savefig(f"results/bias/RON_hist_iso{iso}_ADU.png")
+    plt.savefig(results/f"bias/RON_hist_iso{iso}_ADU.png")
     plt.close()
 
     gain = gain_table[1, iso]
@@ -40,7 +41,7 @@ for ind in (high_iso, low_iso):
     plt.ylabel("Frequency")
     plt.ylim(0.9, std.size)
     plt.grid(ls="--")
-    plt.savefig(f"results/bias/RON_hist_iso{iso}_e.png")
+    plt.savefig(results/f"bias/RON_hist_iso{iso}_e.png")
     plt.close()
 
     std_RGBG, _= raw.pull_apart(std, colours)
@@ -59,13 +60,13 @@ for ind in (high_iso, low_iso):
     axs[2].set_xlabel("Read noise (e$^-$)")
     axs[0].set_yscale("log")
     axs[1].set_ylabel("Probability density")
-    fig.savefig(f"results/bias/RON_hist_iso{iso}_e_colour.png")
+    fig.savefig(results/f"bias/RON_hist_iso{iso}_e_colour.png")
     plt.close()
 
     gauss = gaussMd(std, sigma=10)
 
-    plot.show_image(gauss, colorbar_label="Read noise (e$^-$)", saveto=f"results/bias/RON_gauss_iso{iso}.png")
+    plot.show_image(gauss, colorbar_label="Read noise (e$^-$)", saveto=results/f"bias/RON_gauss_iso{iso}.png")
 
     for j, c in enumerate("RGBG"):
         X = "2" if j == 3 else ""
-        plot.show_image(gauss_RGBG[j], colorbar_label="Read noise (e$^-$)", saveto=f"results/bias/RON_gauss_iso{iso}_{c}{X}.png", colour=c, vmin=vmin, vmax=vmax)
+        plot.show_image(gauss_RGBG[j], colorbar_label="Read noise (e$^-$)", saveto=results/f"bias/RON_gauss_iso{iso}_{c}{X}.png", colour=c, vmin=vmin, vmax=vmax)
