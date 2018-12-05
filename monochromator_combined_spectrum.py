af2f1a0d3679f5f484b8e06db1cbd6ac02f6fad7
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-12-05 12:04:36 +0000

    Combined spectra

---
+++ monochromator_combined_spectrum.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sys import argv
+from phonecal import io, raw, plot
+from matplotlib import pyplot as plt
+
+folder, wvl1, wvl2 = io.path_from_input(argv)
+wvl1 = float(wvl1.stem) ; wvl2 = float(wvl2.stem)
+root, images, stacks, products, results = io.folders(folder)
+phone = io.read_json(root/"info.json")
+
+colours = io.load_colour(stacks)
+
+folders = folder.glob("*")
+
+def load_spectrum(subfolder):
+    mean_files = sorted(subfolder.glob("*_mean.npy"))
+    stds_files = sorted(subfolder.glob("*_stds.npy"))
+    assert len(mean_files) == len(stds_files)
+
+    wvls  = np.zeros((len(mean_files)))
+    means = np.zeros((len(mean_files), 4))
+    stds  = means.copy()
+    for j, (mean_file, stds_file) in enumerate(zip(mean_files, stds_files)):
+        m = np.load(mean_file)
+        mean_RGBG, _ = raw.pull_apart(m, colours)
+        sub = mean_RGBG[:,756-50:756+51,1008-50:1008+51]
+        wvls[j] = mean_file.stem.split("_")[0]
+        means[j] = sub.mean(axis=(1,2))
+        stds[j] = sub.std(axis=(1,2))
+        print(wvls[j])
+    print(subfolder)
+    spectrum = [wvls, means, stds]
+    return spectrum
+
+spectra = [load_spectrum(subfolder) for subfolder in folders]
