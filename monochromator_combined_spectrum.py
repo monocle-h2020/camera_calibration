6805dee69d102507fdf28d71fe0231ca281d8c00
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-12-12 12:21:19 +0100

    Combine calibrated spectra

--- monochromator_combined_spectrum.py
+++ monochromator_combined_spectrum.py
@@ -59,11 +59,6 @@ for i, spec in enumerate(spectra):
     max_in_all = np.where(all_wvl == max_wvl)[0][0]
     all_means[i][min_in_all:max_in_all+1] = spec[:,1:5]
     all_stds[i][min_in_all:max_in_all+1] = spec[:,5:]
-    if i >= 1:
-        max_this = np.nanargmax(all_means[i], axis=0)
-        for j in range(4):
-            comparison = np.nanargmax(all_means[:i, max_this[j], j])
-            norms[i,j] = all_means[i,max_this[j], j] / all_means[comparison, max_this[j], j] * norms[comparison,j]
 
 plt.figure(figsize=(10,5))
 for mean, std in zip(all_means, all_stds):
@@ -80,18 +75,44 @@ plt.savefig(results/"spectral_response/raw_spectra.pdf")
 plt.show()
 plt.close()
 
-for i, (mean, std, cal) in enumerate(zip(all_means, all_stds, cals)):
-    calibration = np.array([cals[0]])
+all_calibrated = all_means.copy()
+all_calibrated[:] = np.nan
 
-#for j, c in enumerate("rgb"):
-#    plt.plot(spectra[0][:,0], spectra[0][:,j+1], c='k') ; plt.plot(spectra[0][:,0], spectra[0][:,j+1] / cal[1,::2], c=c)
-#    plt.show()
+for i, (mean, std, cal) in enumerate(zip(all_means, all_stds, cals)):
+    calibrated = mean.copy() ; calibrated[:] = np.nan
+    overlap, cal_indices, all_wvl_indices = np.intersect1d(cal[0], all_wvl, return_indices=True)
+    calibrated[all_wvl_indices] = mean[all_wvl_indices] / cal[1, cal_indices, np.newaxis]
+    all_calibrated[i] = calibrated
 
 plt.figure(figsize=(10,5))
-for mean, std, norm in zip(all_means, all_stds, norms):
+for mean, std in zip(all_calibrated, all_stds):
     for j, c in enumerate("rgby"):
-        plt.plot(all_wvl, mean[:,j]/norm[j], c=c)
-        plt.fill_between(all_wvl, (mean[:,j]-std[:,j])/norm[j], (mean[:,j]+std[:,j])/norm[j], color=c, alpha=0.3)
+        plt.plot(all_wvl, mean[:,j], c=c)
+        plt.fill_between(all_wvl, mean[:,j]-std[:,j], mean[:,j]+std[:,j], color=c, alpha=0.3)
+    plt.xticks(np.arange(0,1000,50))
+    plt.xlim(wvl1,wvl2)
+    plt.xlabel("Wavelength (nm)")
+    plt.ylabel("Spectral response (ADU)")
+    plt.ylim(ymin=0)
+plt.title(f"{phone['device']['name']}: Calibrated spectral curves")
+plt.savefig(results/"spectral_response/calibrated_spectra.pdf")
+plt.show()
+plt.close()
+
+for i, spec in enumerate(spectra):
+    if i >= 1:
+        max_this = np.nanargmax(all_calibrated[i], axis=0)
+        for j in range(4):
+            comparison = np.nanargmax(all_calibrated[:i, max_this[j], j])
+            norms[i,j] = all_calibrated[i,max_this[j], j] / all_calibrated[comparison, max_this[j], j] * norms[comparison,j]
+
+all_means_normalised = all_calibrated / norms[:, np.newaxis]
+
+plt.figure(figsize=(10,5))
+for mean, std, norm in zip(all_means_normalised, all_stds, norms):
+    for j, c in enumerate("rgby"):
+        plt.plot(all_wvl, mean[:,j], c=c)
+        plt.fill_between(all_wvl, mean[:,j]-std[:,j], mean[:,j]+std[:,j], color=c, alpha=0.3)
     plt.xticks(np.arange(0,1000,50))
     plt.xlim(wvl1,wvl2)
     plt.xlabel("Wavelength (nm)")
@@ -101,8 +122,6 @@ plt.title(f"{phone['device']['name']}: Normalised spectral curves")
 plt.savefig(results/"spectral_response/normalised_spectra.pdf")
 plt.show()
 plt.close()
-
-all_means_normalised = all_means / norms[:, np.newaxis]
 
 flat_means = np.nanmean(all_means_normalised, axis=0)
 flat_stds  = np.nanstd(all_means_normalised, axis=0)
