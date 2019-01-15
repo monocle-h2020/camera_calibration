886cff445d18d548cf22ffd851e5451d45bc6220
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-01-15 22:29:02 +0100

    Exposure time method

---
+++ linearity_calculate_R2_time.py
@@ -0,0 +1,40 @@
+import numpy as np
+from sys import argv
+from phonecal import io
+from phonecal.raw import pull_apart
+from phonecal.general import Rsquare
+from phonecal.gain import malus, malus_error
+
+folder = io.path_from_input(argv)
+root, images, stacks, products, results = io.folders(folder)
+phone = io.read_json(root/"info.json")
+
+times, means = io.load_means (folder, retrieve_value=io.split_exposure_time)
+print("Read means")
+colours      = io.load_colour(stacks)
+
+max_value = 2**phone["camera"]["bits"]
+saturation = 0.95 * max_value
+
+def linear_R2(x, y, saturate=4000):
+    ind = np.where(y < saturate)
+    p = np.polyfit(x[ind], y[ind], 1)
+    pv = np.polyval(p, x[ind])
+    R2 = Rsquare(y[ind], pv)
+    return R2
+
+print("Doing R^2 comparison...")
+
+saturated = []
+R2 = np.zeros(means.shape[1:])
+for i in range(means.shape[1]):
+    for j in range(means.shape[2]):
+        try:
+            R2[i,j] = linear_R2(times, means[:,i,j], saturate=saturation)
+        except TypeError:  # if fully saturated
+            R2[i,j] = np.nan
+            saturated.append((i,j))
+    if i%5 == 0:
+        print(f"{i/means.shape[1]*100:.1f}%")
+
+np.save(products/"linearity_R2.npy", R2)
