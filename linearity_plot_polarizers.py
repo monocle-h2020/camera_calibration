561001b7ff4a7dbd5292e7e00b1ac3d360d29395
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-01-16 13:51:18 +0100

    Refactoriserated linearity plotting

--- linearity_plot_polarizers.py
+++ linearity_plot_polarizers.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sys import argv
 from matplotlib import pyplot as plt
-from phonecal import io
+from phonecal import io, plot
 from phonecal.linearity import malus, malus_error
 
 folder = io.path_from_input(argv)
@@ -30,33 +30,4 @@ intensities_errors = malus_error(angles, offset_angle, sigma_angle0=1, sigma_ang
 
 max_value = 2**phone["camera"]["bits"]
 
-for j in range(4):
-    colour_index = colours_here[j]
-    colour = "rgbg"[colour_index]
-    if colour_index < 3:
-        i = colour_index
-        label = colour
-    else:
-        i = 1
-        label = "g2"
-    mean_dng =  means[:, j]
-    mean_jpg = jmeans[:, j, i]
-
-    fig, ax = plt.subplots(1, 1, figsize=(3.3,2), tight_layout=True)
-    ax.errorbar(intensities, mean_jpg, xerr=intensities_errors, fmt=f"{colour}o", ms=3)
-    ax.set_xlim(-0.02, 1.02)
-    ax.set_ylim(0, 255*1.02)
-    ax.set_xticks(np.arange(0,1.2,0.2))
-    ax.set_yticks(np.arange(0, 255, 50))
-    ax2 = ax.twinx()
-    ax2.errorbar(intensities, mean_dng, xerr=intensities_errors, fmt="ko", ms=3)
-    ax2.set_ylim(0, max_value*1.02)
-    ax2.locator_params(axis="y", nbins=5)
-    jpeglabel = ax.set_ylabel("JPEG value")
-    jpeglabel.set_color(colour)
-    ax.tick_params(axis="y", colors=colour)
-    ax2.set_ylabel("RAW value")
-    ax.set_xlabel("Relative incident intensity")
-    fig.savefig(results/f"linearity/linearity_DNG_JPEG_singlepixel_{label}.pdf")
-    plt.close()
-    print(f"Plotted pixel {j} ({label})")
+plot.plot_linearity_dng_jpg(intensities, means, jmeans, colours_here, intensities_errors=intensities_errors, max_value=max_value, savefolder=results/"linearity")
--- phonecal/plot.py
+++ phonecal/plot.py
@@ -220,4 +220,39 @@ def hist_bias_ron_kRGB(data_RGBG, xlim=(0, 30), nrbins=500, xlabel="Bias (ADU)",
     axs[0].set_yscale("log")
     axs[2].set_ylabel("Probability density")
     _saveshow(saveto)
-    
+
+def plot_linearity_dng_jpg(intensities, means, jmeans, colours_here, intensities_errors=None, max_value=4095, savefolder=None):
+    for j in range(4):
+        colour_index = colours_here[j]
+        colour = "rgbg"[colour_index]
+        if colour_index < 3:
+            i = colour_index
+            label = colour
+        else:
+            i = 1
+            label = "g2"
+        try:
+            saveto = savefolder/f"linearity_DNG_JPEG_singlepixel_{label}.pdf"
+        except:
+            saveto = None
+
+        mean_dng =  means[:, j]
+        mean_jpg = jmeans[:, j, i]
+
+        fig, ax = plt.subplots(1, 1, figsize=(3.3,2), tight_layout=True)
+        ax.errorbar(intensities, mean_jpg, xerr=intensities_errors, fmt=f"{colour}o", ms=3)
+        ax.set_xlim(-0.02, 1.02)
+        ax.set_ylim(0, 255*1.02)
+        ax.set_xticks(np.arange(0,1.2,0.2))
+        ax.set_yticks(np.arange(0, 255, 50))
+        ax2 = ax.twinx()
+        ax2.errorbar(intensities, mean_dng, xerr=intensities_errors, fmt="ko", ms=3)
+        ax2.set_ylim(0, max_value*1.02)
+        ax2.locator_params(axis="y", nbins=5)
+        jpeglabel = ax.set_ylabel("JPEG value")
+        jpeglabel.set_color(colour)
+        ax.tick_params(axis="y", colors=colour)
+        ax2.set_ylabel("RAW value")
+        ax.set_xlabel("Relative incident intensity")
+        _saveshow(saveto)
+        print(f"Plotted pixel {j} ({label})")
