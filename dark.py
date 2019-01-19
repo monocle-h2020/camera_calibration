8c83e84d6bf6966ff6930b870315bd8b6a19d585
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-01-19 16:03:39 +0100

    Renamed ISO variable

--- dark.py
+++ dark.py
@@ -14,8 +14,6 @@ print("Loaded means")
 colours     = io.load_colour(stacks)
 print(f"Loaded data: {len(times)} exposure times")
 
-raise Exception
-
 mean_mean = means.mean(axis=(1,2))
 mean_std  = means.std (axis=(1,2))
 mean_err  = mean_std / np.sqrt(means[0].size - 1)
@@ -40,9 +38,11 @@ plt.xlim(0, time_max)
 plt.ticklabel_format(useOffset=False)
 plt.title(f"Dark current ensemble\n$R^2 = {R2:.3f}$")
 #plt.legend(loc="lower right")
-plt.savefig(results/f"dark/ensemble_iso{iso}.pdf")
+plt.savefig(results/f"dark/ensemble_iso{ISO}.pdf")
 plt.close()
 print("Saved ensemble scatter plot")
+
+raise Exception
 
 mean_reshaped = means.reshape((means.shape[0], -1))  # as list
 fit_separate = np.polyfit(times, mean_reshaped, 1)  # linear fit to every pixel
@@ -51,11 +51,11 @@ dark_separate, bias_separate = fit_separate
 dark_reshaped = dark_separate.reshape(means[0].shape)
 
 dark_gauss = gaussMd(dark_reshaped, 10)
-plot.show_image(dark_gauss, colorbar_label="Dark current (ADU/s)", saveto=results/f"dark/map_iso{iso}.pdf")
+plot.show_image(dark_gauss, colorbar_label="Dark current (ADU/s)", saveto=results/f"dark/map_iso{ISO}.pdf")
 print("Saved Gauss map")
 
 dark_RGBG, _= raw.pull_apart(dark_reshaped, colours)
-plot.hist_bias_ron_kRGB(dark_RGBG, xlim=(-25, 50), xlabel="Dark current (ADU/s)", saveto=results/f"dark/histogram_RGB_iso{iso}.pdf")
+plot.hist_bias_ron_kRGB(dark_RGBG, xlim=(-25, 50), xlabel="Dark current (ADU/s)", saveto=results/f"dark/histogram_RGB_iso{ISO}.pdf")
 del dark_RGBG
 print("Saved RGB histogram")
 
@@ -64,7 +64,7 @@ plt.hist(dark_separate, bins=250, color='k', edgecolor='k')
 plt.yscale("log")
 plt.xlabel("Dark current (ADU/s)")
 plt.ylabel("Frequency")
-plt.savefig(results/f"dark/histogram_iso{iso}.pdf")
+plt.savefig(results/f"dark/histogram_iso{ISO}.pdf")
 plt.close()
 print("Saved ADU histogram")
 
