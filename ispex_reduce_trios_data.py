fea4940e6234cc3a978168b1dee479bdd3a6a339
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-01-21 15:01:57 +0100

    Cleaner table conversion

--- ispex_reduce_trios_data.py
+++ ispex_reduce_trios_data.py
@@ -17,13 +17,33 @@ def get_time(c):
     except:
         return t
 
+def to_float(x):
+    try:
+        return float(x)
+    except ValueError:
+        return np.nan
+
 file = io.path_from_input(argv)
-
 cal = np.genfromtxt(file, delimiter=",", dtype=str)
-cal[cal == ""] = "nan"
-cal[..., 6] = [int(a, 16) for a in cal[..., 6]]
-cal[..., 0] = [get_time(c) for c in cal[...,0]]
-cal[..., 1] = [get_time(c) for c in cal[...,1]]
-cal2 = cal.astype(np.float64)
 
-reflected = cal[::3]
+wavelengths = np.arange(320, 956.9, 3.3)
+
+# 0: Trigger_time_PC
+# 1: Trigger_time_GPS
+# 2: Latitude
+# 3: Longitude
+# 4: Speed
+# 5: Heading
+# 6: Sensor_ID
+# 7: Integration_time
+
+table = np.tile(np.nan, cal.shape)
+table[:, 0] = [get_time(c) for c in cal[:, 0]]
+table[:, 1] = [get_time(c) for c in cal[:, 1]]
+for j in range(2,6):
+    table[:, j] = [to_float(x) for x in cal[:, j]]
+# IDs: 20728 = sky-radiance Ls ; 34251 = downwelling Ed ; 34252 = upwelling Lu
+table[:, 6] = [int(ID, 16) for ID in cal[:, 6]]
+table[:, 7:] = cal[:, 7:]
+
+upwelling = table[table[:, 6] == 34252.]
