18e8e512de5a5c5f085a3027ae6bfe36771dca11
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-01-21 19:00:22 +0100

    Use different legacy script as base

--- ispex_calculate_spectrum.py
+++ ispex_calculate_spectrum.py
@@ -1,48 +1,45 @@
-from matplotlib import pyplot as plt
 import numpy as np
+import rawpy
 from sys import argv
+from matplotlib import pyplot as plt, patheffects as pe
+from phonecal.general import gauss_filter
+from phonecal import raw, plot, io, wavelength
 
-from phonecal import general, io, plot, wavelength
-from phonecal.general import x, y, y_thin, y_thick, x_spectrum
+LUT = np.load("results/gain_new/LUT.npy")
 
 filename = argv[1]
+handle = "_".join(filename.split("/")).split(".")[0]
 
-data = io.load_dng(filename)
+ISO = int(argv[2])  # temporary to test Gain
+texp_inv = int(argv[3])  # temporary to test Gain
+
+img = io.load_dng_raw(filename)
 exif = io.load_exif(filename)
+image_cut  = raw.cut_out_spectrum(img.raw_image)
+colors_cut = raw.cut_out_spectrum(img.raw_colors)
 
-thick, thin = general.split_spectrum(data)
+RGBG, offsets = raw.pull_apart(image_cut, colors_cut)
+plot.RGBG_stacked(RGBG, extent=(raw.xmin, raw.xmax, raw.ymax, raw.ymin), show_axes=True, saveto="RGBG_cutout.png")
 
-plot.plot_photo(thick, extent=(*y_thick, *x_spectrum[::-1]))
-plot.plot_photo(thin , extent=(*y_thin , *x_spectrum[::-1]))
+coefficients = wavelength.load_coefficients("wavelength_solution.npy")
+wavelengths_cut = wavelength.calculate_wavelengths(coefficients, raw.x, raw.y)
+wavelengths_split, offsets = raw.pull_apart(wavelengths_cut, colors_cut)
 
-thickF = general.gauss_filter(thick)
-thinF  = general.gauss_filter(thin )
+lambdarange, all_interpolated = wavelength.interpolate_multi(wavelengths_split, RGBG)
+plot.RGBG_stacked(all_interpolated.swapaxes(1,2), extent=(lambdarange[0], lambdarange[-1], raw.ymax, raw.ymin), show_axes=True, xlabel="$\lambda$ (nm)", aspect=0.5 * len(lambdarange) / len(raw.x), saveto="RGBG_cutout_corrected.png")
+plot.RGBG_stacked_with_graph(all_interpolated, x=lambdarange, extent=(lambdarange[0], lambdarange[-1], raw.ymax, raw.ymin), xlabel="$\lambda$ (nm)", aspect=0.5 * len(lambdarange) / len(raw.x), saveto="RGBG_cutout_corrected_spectrum.png")
 
-plot.plot_photo(thickF, extent=(*y_thick, *x_spectrum[::-1]))
-plot.plot_photo(thinF , extent=(*y_thin , *x_spectrum[::-1]))
+plot.RGBG(RGBG, vmax=800, saveto="RGBG_split.png", size=30)
+plot.RGBG(all_interpolated.swapaxes(1,2), vmax=800, saveto="RGBG_split_interpolated.png", size=30)
 
-y_example = 100
-plot.plot_spectrum(general.x, thick[:, y_example], xlabel="$x$ (px)", title=f"RGB values at $y = {y_example+y_thick[0]}$", xlim=x_spectrum, ylim=(0,255))
-plot.plot_spectrum(general.x, thickF[:, y_example], xlabel="$x$ (px)", title=f"RGB values at $y = {y_example+y_thick[0]}$", xlim=x_spectrum, ylim=(0,255))
-plot.plot_spectrum(general.x, thin[:, y_example], xlabel="$x$ (px)", title=f"RGB values at $y = {y_example+y_thin[0]}$", xlim=x_spectrum, ylim=(0,255))
-plot.plot_spectrum(general.x, thinF[:, y_example], xlabel="$x$ (px)", title=f"RGB values at $y = {y_example+y_thin[0]}$", xlim=x_spectrum, ylim=(0,255))
+stacked = wavelength.stack(lambdarange, all_interpolated)
+stacked[1:] -= 528
+np.save("results/spectra/"+handle+"_spectrum.npy", stacked)
 
-coefficients = wavelength.load_coefficients("wavelength.npy")
+plot.plot_spectrum(stacked[0], stacked[1:], saveto="results/spectra/"+handle+"_spectrum_rgb.png",
+                   xlim=(340, 760), ylim=(0, None), title=exif["Image DateTime"].values)
 
-wavelengths, intensity_thick = wavelength.stack(x, thickF, coefficients, yoffset=y_thick[0])
-wavelengths, intensity_thin  = wavelength.stack(x, thinF , coefficients, yoffset=y_thin[0] )
-
-plot.plot_spectrum(wavelengths, intensity_thick, title="Stacked thick RGB spectrum", ylabel="C (au)", ylim=(0, None), xlim=wavelength.wavelength_limits)
-plot.plot_spectrum(wavelengths, intensity_thin , title="Stacked thin RGB spectrum" , ylabel="C (au)", ylim=(0, None), xlim=wavelength.wavelength_limits)
-
-wb = general.find_white_balance(data)
-intensity_thick /= wb
-intensity_thin  /= wb
-plot.plot_spectrum(wavelengths, intensity_thick, title="Stacked thick RGB spectrum (post-WB)", ylabel="C (au)", ylim=(0, None), xlim=wavelength.wavelength_limits)
-plot.plot_spectrum(wavelengths, intensity_thin , title="Stacked thin RGB spectrum (post-WB)" , ylabel="C (au)", ylim=(0, None), xlim=wavelength.wavelength_limits)
-
-filter_curves = np.load("filtercurves.npy")
-intensity_thick /= filter_curves
-intensity_thin  /= filter_curves
-plot.plot_spectrum(wavelengths, intensity_thick, title="Stacked thick RGB spectrum (post-FC)", ylabel="C (au)", ylim=(0, None), xlim=wavelength.wavelength_limits)
-plot.plot_spectrum(wavelengths, intensity_thin , title="Stacked thin RGB spectrum (post-FC)" , ylabel="C (au)", ylim=(0, None), xlim=wavelength.wavelength_limits)
+gain = LUT[1, ISO]
+stacked[1:] *= gain * texp_inv  # convert ADU to e-/s
+plot.plot_spectrum(stacked[0], stacked[1:], xlim=(340, 760), ylim=(0, 70), title=f"ISO {ISO} ; 1/t_exp {texp_inv:.1f}",
+                   ylabel="$e^-/s$", saveto=f"ISO_{ISO}_texp_{texp_inv:.1f}.png")
