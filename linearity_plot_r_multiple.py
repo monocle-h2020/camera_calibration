d3486edb33469cc3a68e001a2654b31592d5775f
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-02-22 00:43:23 +0100

    Warn about number of r<0.9 pixels

--- linearity_plot_r_multiple.py
+++ linearity_plot_r_multiple.py
@@ -14,13 +14,20 @@ cameras = [io.read_json(root/"info.json")["device"]["name"] for root in roots]
 r_raw  = [np.load(raw_path ) for raw_path  in r_raw_paths ]
 r_jpeg = [np.load(jpeg_path) for jpeg_path in r_jpeg_paths]
 
-bins = np.linspace(0.9, 1.0, 1000)
+lower_limit = 0.9
+
+bins = np.linspace(lower_limit, 1.0, 1000)
 
 fig, axs = plt.subplots(nrows=len(r_raw), sharex=True, sharey=True, squeeze=True, tight_layout=True, figsize=(4,1.3*len(r_raw)), gridspec_kw={"wspace":0, "hspace":0})
 for ax, raw, jpeg, camera in zip(axs, r_raw, r_jpeg, cameras):
+    print(camera)
+    below_limit = np.where(raw < lower_limit)[0]
+    print(f"RAW pixels with r < {lower_limit}: {len(below_limit)}")
     ax.hist(raw.ravel(), bins=bins, color='k')
-    for j, c in enumerate("rgb"):
-        ax.hist(jpeg[j].ravel(), bins=bins, color=c, alpha=0.7)
+    for jpeg_c, c in zip(jpeg, "rgb"):
+        below_limit = np.where(jpeg_c < lower_limit)[0]
+        print(f"JPEG {c} pixels with r < {lower_limit}: {len(below_limit)}")
+        ax.hist(jpeg_c.ravel(), bins=bins, color=c, alpha=0.7)
     ax.set_ylabel(camera)
 axs[0] .set_xlim(0.9, 1)
 axs[0] .set_yscale("log")
