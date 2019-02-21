36be28ee7a7704a15596b7d2d74d3a9d9aad0aaa
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-02-21 20:53:12 +0100

    Compare RAW/JPEG r values

--- linearity_plot_r.py
+++ linearity_plot_r.py
@@ -15,17 +15,29 @@ else:
     jpeg = True
 r_ravel = r.ravel()
 if jpeg:
-    r_jpeg_ravel = r_jpeg.reshape(3, -1)
+    r_jpeg_ravel = [r_colour.ravel() for r_colour in r_jpeg]
 print("Read Pearson r")
 
 nans = np.where((np.isnan(r_ravel)) | np.isinf(r_ravel))
 print(f"Number of megapixels: {len(r_ravel)/1e6:.1f} ; Number of NaN/inf: {len(nans[0])}")
-
 r_ravel = np.delete(r_ravel, nans)
+if jpeg:
+    for j in range(3):
+        nans = np.where((np.isnan(r_jpeg_ravel[j])) | np.isinf(r_jpeg_ravel[j]))
+        r_jpeg_ravel[j] = np.delete(r_jpeg_ravel[j], nans)
 
-print(f"Lowest: {r_ravel.min():.5f}")
+print("Percentile: RAW r    JPEG_R   JPEG_G   JPEG_B")
+print(f"    Lowest: {r_ravel.min():.5f}", end="  ")
+if jpeg:
+    for j in range(3):
+        print(f"{r_jpeg_ravel[j].min():.5f}", end="  ")
+print()
 for percentage in [0.1, 1, 5, 50, 90, 95, 99, 99.9]:
-    print(f"{percentage:>5.1f}%: {np.percentile(r_ravel, percentage):.5f}")
+    print(f"{percentage:>9.1f}%: {np.percentile(r_ravel, percentage):.5f}", end="  ")
+    if jpeg:
+        for j in range(3):
+            print(f"{np.percentile(r_jpeg_ravel[j], percentage):.5f}", end="  ")
+    print()
 
 percentile = np.percentile(r_ravel, 0.5)
 
@@ -73,3 +85,17 @@ plt.ylim(ymin=0.9)
 plt.savefig(results/"linearity/r_log.pdf")
 plt.close()
 print("Made logarithmic histogram")
+
+if jpeg:
+    bins_new = np.linspace(0.9, 1.0, 250)
+    plt.figure(tight_layout=True, figsize=(5,4))
+    plt.hist(r_ravel, bins=bins_new, color='k', density=True)
+    for j, c in enumerate("rgb"):
+        plt.hist(r_jpeg_ravel[j], bins=bins_new, color=c, alpha=0.7, density=True)
+    plt.xlabel("Pearson $r$")
+    plt.ylabel("Density")
+    plt.xlim(0.9, 1)
+    plt.xticks(rotation=30)
+    plt.savefig(results/"linearity/r_raw_jpeg.pdf")
+    plt.close()
+    print("Made RAW/JPEG histogram")
