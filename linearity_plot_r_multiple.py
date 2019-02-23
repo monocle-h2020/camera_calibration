8d272a97bc01378c217f7d5546e683b4c6f1af86
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-02-23 17:36:33 +0100

    Ignore NaN

--- linearity_plot_r_multiple.py
+++ linearity_plot_r_multiple.py
@@ -19,12 +19,14 @@ lower_limit = 0.9
 bins = np.linspace(lower_limit, 1.0, 1000)
 
 fig, axs = plt.subplots(nrows=len(r_raw), sharex=True, sharey=True, squeeze=True, tight_layout=True, figsize=(4,1.3*len(r_raw)), gridspec_kw={"wspace":0, "hspace":0})
-for ax, raw, jpeg, camera in zip(axs, r_raw, r_jpeg, cameras):
+for ax, raw_, jpeg_, camera in zip(axs, r_raw, r_jpeg, cameras):
+    raw = raw_[~np.isnan(raw_)]
     print(camera)
     below_limit = np.where(raw < lower_limit)[0]
     print(f"RAW pixels with r < {lower_limit}: {len(below_limit)}")
     ax.hist(raw.ravel(), bins=bins, color='k')
-    for jpeg_c, c in zip(jpeg, "rgb"):
+    for j_, c in zip(jpeg_, "rgb"):
+        jpeg_c = j_[~np.isnan(j_)]
         below_limit = np.where(jpeg_c < lower_limit)[0]
         print(f"JPEG {c} pixels with r < {lower_limit}: {len(below_limit)}")
         ax.hist(jpeg_c.ravel(), bins=bins, color=c, alpha=0.7)
@@ -37,5 +39,5 @@ axs[0] .set_yscale("log")
 axs[0] .set_ylim(ymin=0.9)
 axs[-1].set_xlabel("Pearson $r$")
 fig.savefig("results/linearity.pdf")
-fig.show()
+plt.show()
 plt.close()
