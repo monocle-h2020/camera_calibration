e3bb899385ec87089ef69cf25099e7bf6687b6ff
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-02-23 18:28:00 +0100

    Allow non-JPEG

--- linearity_plot_r_multiple.py
+++ linearity_plot_r_multiple.py
@@ -11,8 +11,15 @@ r_jpeg_paths = [root/"products/linearity_pearson_r_jpeg.npy" for root in roots]
 
 cameras = [io.read_json(root/"info.json")["device"]["name"] for root in roots]
 
-r_raw  = [np.load(raw_path ) for raw_path  in r_raw_paths ]
-r_jpeg = [np.load(jpeg_path) for jpeg_path in r_jpeg_paths]
+def load_jpeg(path):
+    try:
+        jpeg = np.load(path)
+    except FileNotFoundError:
+        jpeg = None
+    return jpeg
+
+r_raw  = [  np.load(raw_path ) for raw_path  in r_raw_paths ]
+r_jpeg = [load_jpeg(jpeg_path) for jpeg_path in r_jpeg_paths]
 
 lower_limit = 0.9
 
@@ -25,11 +32,12 @@ for ax, raw_, jpeg_, camera in zip(axs, r_raw, r_jpeg, cameras):
     below_limit = np.where(raw < lower_limit)[0]
     print(f"RAW pixels with r < {lower_limit}: {len(below_limit)}")
     ax.hist(raw.ravel(), bins=bins, color='k')
-    for j_, c in zip(jpeg_, "rgb"):
-        jpeg_c = j_[~np.isnan(j_)]
-        below_limit = np.where(jpeg_c < lower_limit)[0]
-        print(f"JPEG {c} pixels with r < {lower_limit}: {len(below_limit)}")
-        ax.hist(jpeg_c.ravel(), bins=bins, color=c, alpha=0.7)
+    if jpeg_ is not None:
+        for j_, c in zip(jpeg_, "rgb"):
+            jpeg_c = j_[~np.isnan(j_)]
+            below_limit = np.where(jpeg_c < lower_limit)[0]
+            print(f"JPEG {c} pixels with r < {lower_limit}: {len(below_limit)}")
+            ax.hist(jpeg_c.ravel(), bins=bins, color=c, alpha=0.7)
     ax.set_ylabel(camera)
     ax.axvline(lin.linearity_limit, c='k', ls="--")
     bad_pixels = np.where(raw < lin.linearity_limit)[0]
