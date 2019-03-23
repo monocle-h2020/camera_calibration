708b84a4c29b9411de9679cd946d714894d4930a
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-03-23 16:39:02 +0100

    Smaller & Log scale

--- linearity_jpeg_sRGB_plot.py
+++ linearity_jpeg_sRGB_plot.py
@@ -10,13 +10,15 @@ phone = io.read_json(root/"info.json")
 normalisations, offsets, gammas, R2s = [np.load(results/f"linearity/{label_simple}.npy") for label_simple in ["normalization", "offset", "gamma", "R2"]]
 
 for param, label, label_simple in zip([normalisations, offsets, gammas, R2s], ["Input normalization", "Offset (ADU)", "Gamma", "$R^2$"], ["normalization", "offset", "gamma", "R2"]):
-    plt.figure(figsize=(4,4), tight_layout=True)
+    plt.figure(figsize=(4,2), tight_layout=True)
     for j, c in enumerate("rgb"):
         P = param[...,j].ravel()
         P = P[~np.isnan(P)]
         plt.hist(P, bins=250, color=c, alpha=0.7)
     plt.xlabel(label)
     plt.ylabel("Frequency")
+    plt.xticks(rotation=20)
+    plt.yscale("log")
     plt.title(f"{len(P)} pixels ({len(P)/(param.shape[0] * param.shape[1])*100:.1f}%)")
     plt.savefig(results/f"linearity/jpeg_{label_simple}.pdf")
     #plt.show()
