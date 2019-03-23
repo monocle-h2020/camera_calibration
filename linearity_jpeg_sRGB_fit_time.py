82e1e98de9c192dee85df7e0fdc62fbfeee45dfa
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-03-23 17:23:40 +0100

    Remove offset

--- linearity_jpeg_sRGB_fit_time.py
+++ linearity_jpeg_sRGB_fit_time.py
@@ -15,7 +15,7 @@ saturation = 0.95 * max_value
 
 print("Fitting sRGB...", end=" ", flush=True)
 
-normalisations, offsets, gammas, R2s = lin.fit_sRGB_generic(times, jmeans)
+normalisations, gammas, R2s = lin.fit_sRGB_generic(times, jmeans)
 
 for param, label_simple in zip([normalisations, gammas, R2s], ["normalization", "gamma", "R2"]):
     np.save(results/f"linearity/{label_simple}.npy", param)
