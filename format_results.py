dcef5b943b1ed25c9fca85b097575f3b50c244fc
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-06 15:52:25 +0200

    Imagine misunderstanding documentation this badly

--- format_results.py
+++ format_results.py
@@ -23,13 +23,13 @@ makedirs(save_folder, exist_ok=True)  # create folder if it does not yet exist
 # General properties
 
 generic_header =f"\
-# SPECTACLE data sheet. More information can be found in our paper (https://doi.org/10.1364/OE.27.019075) and on our website (http://spectacle.ddq.nl/). \n\
-# Camera manufacturer: {phone['device']['manufacturer']}\n\
-# Camera device: {phone['device']['name']}\n\
-# Camera product code: {phone['device']['code']}\n\
-# Sensor manufacturer: {phone['camera']['manufacturer']}\n\
-# Sensor series: {phone['camera']['series']}\n\
-# Sensor model: {phone['camera']['model']}\n"
+SPECTACLE data sheet. More information can be found in our paper (https://doi.org/10.1364/OE.27.019075) and on our website (http://spectacle.ddq.nl/). \n\
+Camera manufacturer: {phone['device']['manufacturer']}\n\
+Camera device: {phone['device']['name']}\n\
+Camera product code: {phone['device']['code']}\n\
+Sensor manufacturer: {phone['camera']['manufacturer']}\n\
+Sensor series: {phone['camera']['series']}\n\
+Sensor model: {phone['camera']['model']}\n"
 
 # Linearity
 
@@ -54,6 +54,7 @@ except FileNotFoundError:
     spectral_response = np.tile(np.nan, (9, 156))
 
 spectral_response = spectral_response.T  # transpose to have columns for wavelength, R, G, ...
-header = "wavelength (nm),R,G,B,G2,R_error,G_error,B_error,G2_error"
+header = "This file contains spectral response data. \n\
+wavelength (nm),R,G,B,G2,R_error,G_error,B_error,G2_error"
 
-np.savetxt(save_folder/f"spectral_response_{identifier}.csv", spectral_response, delimiter=",", fmt="%.8f", header=header, comments=generic_header)
+np.savetxt(save_folder/f"spectral_response_{identifier}.csv", spectral_response, delimiter=",", fmt="%.8f", header=generic_header+header)
