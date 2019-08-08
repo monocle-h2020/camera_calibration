8dffa5aa6b67241b226463b3802b178785f88c9a
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-08 15:59:59 +0200

    Function for creating folders

--- spectacle/config.py
+++ spectacle/config.py
@@ -7,10 +7,28 @@ This file is included in .gitignore so you do not accidentally push it to GitHub
 """
 
 from pathlib import Path
+from os import makedirs
 
 # Do not edit these
 home_folder = Path.home()
 
+def create_folders():
+    """
+    Create the specified folders for data (spectacle_folder) and results (results_folder)
+    """
+    try:
+        makedirs(spectacle_folder)
+    except FileExistsError:
+        print(f"Requested SPECTACLE data folder '{spectacle_folder}' already exists.")
+    else:
+        print(f"Created SPECTACLE data folder: {spectacle_folder}")
+
+    try:
+        makedirs(results_folder)
+    except FileExistsError:
+        print(f"Requested SPECTACLE results folder '{results_folder}' already exists.")
+    else:
+        print(f"Created SPECTACLE results folder: {results_folder}")
 
 
 # Edit these with your information if you wish
