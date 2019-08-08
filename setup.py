40431665d599a5cd32be4ec3c6fbab52ca56ad78
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-08 16:47:41 +0200

    Long description

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
       name="spectacle",
       version="0.1",
       description="SPECTACLE camera calibration module",
+      long_description = long_description,
       author="Olivier Burggraaff",
       author_email="burggraaff@strw.leidenuniv.nl",
       packages=["spectacle"],
