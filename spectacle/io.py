2551483d323d4e9cdacd4d60d8d4540773c57691
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-08 16:14:10 +0200

    load_raw_colors consistent with load_raw_image

--- spectacle/io.py
+++ spectacle/io.py
@@ -22,7 +22,10 @@ def load_raw_image(filename):
     img = load_raw_file(filename)
     return img.raw_image
 
-def load_colors(filename):
+def load_raw_colors(filename):
+    """
+    Load a raw file using rawpy's `imread` function. Return only the Bayer colour data.
+    """
     img = load_raw_file(str(filename))
     return img.raw_colors
 
