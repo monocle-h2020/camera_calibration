95484ab8d5aba6d88bddf0f26da252c2a4735456
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-08 14:24:44 +0200

    Module purely for calibrating data

---
+++ spectacle/calibrate.py
@@ -0,0 +1,7 @@
+"""
+Module for calibrating camera data based on calibration data.
+
+If you are only interested in calibrating your data, using previously generated
+calibrations, this is the module to use.
+"""
+
