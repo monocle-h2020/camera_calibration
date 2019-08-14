17831df6ee368c94e7c99e3da4eb08042a08834f
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-14 16:47:43 +0200

    Documentation

--- calibration/gain.py
+++ calibration/gain.py
@@ -1,25 +1,36 @@
+"""
+Create a gain map using gain images, fitting the mean and standard deviation
+against each other. Data for a single ISO speed are loaded and fitted.
+
+Command line arguments:
+    * `folder`: folder containing stacked gain data for a single ISO speed
+"""
+
 import numpy as np
 from sys import argv
 from spectacle import io, calibrate
 
+# Get the data folder from the command line
 folder = io.path_from_input(argv)
 root, images, stacks, products, results = io.folders(folder)
+
+# Get the camera metadata
 phone = io.load_metadata(root)
+fit_max = 0.95 * 2**phone["camera"]["bits"]
+colours = io.load_colour(stacks)
 
-products_gain, results_gain = products/"gain", results/"gain"
+# Get the ISO speed of these data from the folder name
 ISO = io.split_iso(folder)
-print("Loaded information")
+print("Loaded metadata")
 
+# Load the data
 names, means = io.load_means (folder  )
 names, stds  = io.load_stds  (folder  )
-colours      = io.load_colour(stacks  )
 print("Loaded data")
 
 means = calibrate.correct_bias(root, means)
 
 variance = stds**2
-
-fit_max = 0.95 * 2**phone["camera"]["bits"]
 
 gains = np.tile(np.nan, means.shape[1:])
 rons  = gains.copy()
@@ -36,4 +47,4 @@ for i in range(means.shape[1]):
     if i%15:
         print(f"{100 * i / means.shape[1]:.1f}%", end=" ", flush=True)
 
-np.save(products_gain/f"iso{ISO}.npy", gains)
+np.save(root/f"products/gain/iso{ISO}.npy", gains)
