49f53fec82e703716fac3dd8c88b33a45e6fde55
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-14 16:42:19 +0200

    More generic save folder

--- analysis/gain_characterise.py
+++ analysis/gain_characterise.py
@@ -8,7 +8,8 @@ file = io.path_from_input(argv)
 root, images, stacks, products, results = io.folders(file)
 phone = io.load_metadata(root)
 
-products_gain, results_gain = products/"gain", results/"gain"
+savefolder = root/"results/gain"
+
 ISO = io.split_iso(file)
 colours = io.load_colour(stacks)
 print("Loaded information")
@@ -17,11 +18,11 @@ gains = np.load(file)
 gains_RGBG,_ = raw.pull_apart(gains, colours)
 gains_gauss = gauss_nan(gains_RGBG, sigma=(0,5,5))
 
-plot.histogram_RGB(gains_RGBG, xlim=(0,8), xlabel="Gain (ADU/e$^-$)", saveto=results_gain/f"hist_iso{ISO}.pdf")
+plot.histogram_RGB(gains_RGBG, xlim=(0,8), xlabel="Gain (ADU/e$^-$)", saveto=savefolder/f"hist_iso{ISO}.pdf")
 print("Made histogram")
 
 vmin, vmax = np.nanmin(gains_gauss), np.nanmax(gains_gauss)
-plot.show_RGBG(gains_gauss, colorbar_label=25*" "+"Gain (ADU/e$^-$)", vmin=vmin, vmax=vmax, saveto=results_gain/f"map_iso{ISO}.pdf")
+plot.show_RGBG(gains_gauss, colorbar_label=25*" "+"Gain (ADU/e$^-$)", vmin=vmin, vmax=vmax, saveto=savefolder/f"map_iso{ISO}.pdf")
 print("Made map")
 
 gains_combined_gauss = gauss_nan(gains, sigma=10)
@@ -32,7 +33,7 @@ for gauss, label, cm in zip([gains_combined_gauss, *gains_gauss], ["combined", *
     plt.yticks([])
     cbar = plot.colorbar(im)
     cbar.set_label("Gain (ADU/e-)")
-    plt.savefig(results_gain/f"map_iso{ISO}_{label}.pdf")
+    plt.savefig(savefolder/f"map_iso{ISO}_{label}.pdf")
     plt.close()
 print("Made individual maps")
 
@@ -49,6 +50,6 @@ axs[0].set_xlim(0, 3.5)
 axs[0].set_ylim(0, 2.5)
 axs[2].set_xlabel("Gain (ADU/e-)")
 axs[1].set_ylabel("Frequency")
-plt.savefig(results_gain/f"hist_iso{ISO}_RGB.pdf")
+plt.savefig(savefolder/f"hist_iso{ISO}_RGB.pdf")
 plt.close()
 print("Made RGB histogram")
