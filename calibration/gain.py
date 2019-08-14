295018b9751ba3d47269f8633eece491becfa28d
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-14 18:01:00 +0200

    Documentation

--- calibration/gain.py
+++ calibration/gain.py
@@ -24,27 +24,35 @@ ISO = io.split_iso(folder)
 print("Loaded metadata")
 
 # Load the data
-names, means = io.load_means (folder  )
-names, stds  = io.load_stds  (folder  )
+names, means = io.load_means(folder)
+names, stds = io.load_stds(folder)
 print("Loaded data")
 
+# Bias correction
 means = calibrate.correct_bias(root, means)
 
+# Use variance instead of standard deviation
 variance = stds**2
 
-gains = np.tile(np.nan, means.shape[1:])
-rons  = gains.copy()
+# Empty arrays to hold the result
+gain_map = np.tile(np.nan, means.shape[1:])
+readnoise_map = gain_map.copy()
 
+# Loop over the pixels in the array and fit each response individually
 for i in range(means.shape[1]):
     for j in range(means.shape[2]):
         m = means[:,i,j] ; v = variance[:,i,j]
         ind = np.where(m < fit_max)
         try:
-            gains[i,j], rons[i,j] = np.polyfit(m[ind], v[ind], 1, w=1/m[ind])
+            gain_map[i,j], readnoise_map[i,j] = np.polyfit(m[ind], v[ind], 1, w=1/m[ind])
         except:
+            # Keep a NaN value if fitting is not possible for whatever reason
             pass
 
-    if i%15:
+    # Progress counter: give percentage done every 100th row
+    if i%100:
         print(f"{100 * i / means.shape[1]:.1f}%", end=" ", flush=True)
 
-np.save(root/f"products/gain/iso{ISO}.npy", gains)
+save_to = root/f"products/gain/gain_iso{ISO}.npy"
+np.save(save_to, gain_map)
+print(f"Saved result to '{save_to}'")
