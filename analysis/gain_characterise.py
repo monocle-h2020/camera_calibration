8af3b0940ddb8a71360f6d213318eec8a781be03
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-14 16:47:55 +0200

    Consistent vmin/vmax

--- analysis/gain_characterise.py
+++ analysis/gain_characterise.py
@@ -2,7 +2,7 @@ import numpy as np
 from sys import argv
 from matplotlib import pyplot as plt
 from spectacle import raw, io, plot
-from spectacle.general import gauss_nan
+from spectacle.general import gauss_nan, symmetric_percentiles
 
 file = io.path_from_input(argv)
 root, images, stacks, products, results = io.folders(file)
@@ -21,14 +21,14 @@ gains_gauss = gauss_nan(gains_RGBG, sigma=(0,5,5))
 plot.histogram_RGB(gains_RGBG, xlim=(0,8), xlabel="Gain (ADU/e$^-$)", saveto=savefolder/f"hist_iso{ISO}.pdf")
 print("Made histogram")
 
-vmin, vmax = np.nanmin(gains_gauss), np.nanmax(gains_gauss)
+vmin, vmax = symmetric_percentiles(gains_gauss)
 plot.show_RGBG(gains_gauss, colorbar_label=25*" "+"Gain (ADU/e$^-$)", vmin=vmin, vmax=vmax, saveto=savefolder/f"map_iso{ISO}.pdf")
 print("Made map")
 
 gains_combined_gauss = gauss_nan(gains, sigma=10)
 for gauss, label, cm in zip([gains_combined_gauss, *gains_gauss], ["combined", *"RGB", "G2"], [None, "Rr", "Gr", "Br", "Gr"]):
     plt.figure(figsize=(3.3,2.5), tight_layout=True)
-    im = plt.imshow(gauss, cmap=plot.cmaps[cm])
+    im = plt.imshow(gauss, cmap=plot.cmaps[cm], vmin=vmin, vmax=vmax)
     plt.xticks([])
     plt.yticks([])
     cbar = plot.colorbar(im)
