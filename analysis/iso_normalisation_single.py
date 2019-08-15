7904888124b05e110af43494fccff8649f1a1e0f
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-15 16:40:23 +0200

    Single save_to

--- analysis/iso_normalisation_single.py
+++ analysis/iso_normalisation_single.py
@@ -18,7 +18,7 @@ root, images, stacks, products, results = io.folders(file)
 # Get metadata
 phone = io.load_metadata(root)
 iso_max = phone["software"]["ISO max"]
-save_to_folder = root/"results/iso"
+save_to = root/"results/iso/iso_normalisation.pdf"
 
 # Load the normalisation data and look-up table
 lookup_table = np.load(products/"iso_lookup_table.npy")
@@ -26,7 +26,6 @@ data = np.load(products/"iso_data.npy")
 print("Loaded data")
 
 # Plot the normalisation data and look-up table
-save_to_plot = root/"results/iso/iso_normalisation.pdf"
 plt.figure(figsize=(4, 3), tight_layout=True)
 plt.errorbar(data[0], data[1], yerr=data[2], fmt=f"o", c='k')
 plt.plot(*lookup_table, c='k')
@@ -35,6 +34,6 @@ plt.ylabel("Normalisation")
 plt.xlim(0, iso_max*1.05)
 plt.ylim(ymin=0)
 plt.grid(True)
-plt.savefig(save_to_plot)
+plt.savefig(save_to)
 plt.close()
-print(f"Saved plot to '{save_to_plot}'")
+print(f"Saved plot to '{save_to}'")
