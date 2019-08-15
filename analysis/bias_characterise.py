5b3c3dafc9a38c94c67d92527198ac904677f2e1
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-15 13:09:27 +0200

    Documentation & Logical order

--- analysis/bias_characterise.py
+++ analysis/bias_characterise.py
@@ -6,34 +6,48 @@ Command line arguments:
 """
 
 from sys import argv
-from matplotlib import pyplot as plt
 from spectacle import raw, plot, io, analyse
 from spectacle.general import gaussMd
 
+# Get the data folder from the command line
 folder = io.path_from_input(argv)
 root, images, stacks, products, results = io.folders(folder)
+
+# Get metadata
 phone = io.load_metadata(root)
-
-isos, means = io.load_means (folder, retrieve_value=io.split_iso)
-colours     = io.load_colour(stacks)
-
-table = analyse.statistics(means, prefix_column=isos, prefix_column_header="ISO")
-print(table)
-
+colours = io.load_colour(stacks)
 savefolder = results/"bias"
 
+# Load the data
+isos, means = io.load_means(folder, retrieve_value=io.split_iso)
+print("Loaded data")
+
+# Print statistics of the bias at each ISO
+stats = analyse.statistics(means, prefix_column=isos, prefix_column_header="ISO")
+print(stats)
+
+# Range on the x axis for the histograms
 xmax = phone["software"]["bias"] + 25
 xmin = max(phone["software"]["bias"] - 25, 0)
 
+# Loop over the data and make plots at each ISO value
 for iso, mean in zip(isos, means):
-    gauss = gaussMd(mean, sigma=10)
+    # Split the data into the Bayer RGBG2 filters and make an RGB histogram
     mean_RGBG, _ = raw.pull_apart(mean, colours)
+    saveto_histogram = savefolder/f"histogram_iso{iso}.pdf"
+    plot.histogram_RGB(mean_RGBG, xlim=(xmin, xmax), xlabel="Bias (ADU)", saveto=saveto_histogram, nrbins=100)
+    print(f"Saved RGB histogram to '{saveto_histogram}'")
+   
+    # Convolve the maps with a Gaussian kernel
+    gauss_combined = gaussMd(mean, sigma=10)
     gauss_RGBG = gaussMd(mean_RGBG, sigma=(0,5,5))
+    
+    # Find limits for the colorbar, to be used in every map image
     vmin, vmax = gauss_RGBG.min(), gauss_RGBG.max()
-
-    plot.histogram_RGB(mean_RGBG, xlim=(xmin, xmax), xlabel="Bias (ADU)", saveto=savefolder/f"histogram_iso{iso}.pdf", nrbins=100)
-
-    plot.show_image(gauss, colorbar_label="Bias (ADU)", saveto=savefolder/f"gauss_iso{iso}.pdf")
-    plot.show_image_RGBG2(gauss_RGBG, colorbar_label="Bias (ADU)", saveto=savefolder/f"gauss_iso{iso}.pdf", vmin=vmin, vmax=vmax)
-
-    plot.show_RGBG(gauss_RGBG, colorbar_label=25*" "+"Bias (ADU)", saveto=savefolder/f"all_gauss_iso{iso}.pdf", vmin=vmin, vmax=vmax)
+    
+    # Plot images of the Gauss-convolved map
+    saveto_map = savefolder/f"bias_map_iso{iso}.pdf"
+    plot.show_image(gauss_combined, colorbar_label="Bias (ADU)", saveto=saveto_map)
+    plot.show_image_RGBG2(gauss_RGBG, colorbar_label="Bias (ADU)", saveto=saveto_map, vmin=vmin, vmax=vmax)
+    plot.show_RGBG(gauss_RGBG, colorbar_label=25*" "+"Bias (ADU)", saveto=savefolder/f"bias_map_iso{iso}_RGBG2.pdf", vmin=vmin, vmax=vmax)
+    print(f"Saved RGBG2 maps to '{saveto_map}'")
