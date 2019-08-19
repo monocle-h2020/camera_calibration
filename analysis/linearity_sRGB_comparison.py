3451eba2ad1d2f12d0a1f8de950356a98aa4115b
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-19 14:34:18 +0200

    Multiple gamma values simultaneously

--- analysis/linearity_sRGB_comparison.py
+++ analysis/linearity_sRGB_comparison.py
@@ -7,7 +7,8 @@ These calculations are all done for each individual pixel.
 
 Command line arguments:
     * `folder`: the folder containing linearity data stacks.
-    * `gamma`: the gamma value of the sRGB model to compare to the data.
+    * `gamma`: the gamma value of the sRGB model to compare to the data. Any
+    number of gamma values can be provided.
 """
 
 import numpy as np
@@ -15,8 +16,8 @@ from sys import argv
 from spectacle import io, linearity as lin
 
 # Get the data folder from the command line
-folder, gamma = io.path_from_input(argv)
-gamma = float(str(gamma))
+folder, *gammas = io.path_from_input(argv)
+gammas = [float(str(gamma)) for gamma in gammas]
 root, images, stacks, products, results = io.folders(folder)
 save_folder = root/"results/linearity/"
 
@@ -25,12 +26,14 @@ intensities_with_errors, jmeans = io.load_jmeans(folder, retrieve_value=lin.file
 intensities, intensity_errors = intensities_with_errors.T
 print("Loaded data")
 
-# Fit the model
-print(f"Fitting sRGB model with gamma = {gamma} ...")
-normalizations, Rsquares, RMSes, RMSes_relative = lin.sRGB_compare_gamma(intensities, jmeans, gamma=gamma)
+# Loop over the given gamma values
+for gamma in gammas:
+    # Fit the model
+    print(f"\nFitting sRGB model with gamma = {gamma} ...")
+    normalizations, Rsquares, RMSes, RMSes_relative = lin.sRGB_compare_gamma(intensities, jmeans, gamma=gamma)
 
-# Save the output
-for param, label_simple in zip([normalizations, Rsquares, RMSes, RMSes_relative], ["normalization", "R2", "RMS", "RMS_rel"]):
-    save_to = save_folder/f"sRGB_gamma{gamma}_{label_simple}.npy"
-    np.save(save_to, param)
-    print(f"Saved {label_simple} results to '{save_to}'")
+    # Save the output
+    for param, label_simple in zip([normalizations, Rsquares, RMSes, RMSes_relative], ["normalization", "R2", "RMS", "RMS_rel"]):
+        save_to = save_folder/f"sRGB_gamma{gamma}_{label_simple}.npy"
+        np.save(save_to, param)
+        print(f"Saved {label_simple} results to '{save_to}'")
