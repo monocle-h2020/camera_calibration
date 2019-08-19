5122f9366bcfff8cc7f837a49a9567410825c4c7
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-19 15:57:30 +0200

    Documentation

--- analysis/linearity_plot_response.py
+++ analysis/linearity_plot_response.py
@@ -1,17 +1,30 @@
+"""
+Plot the camera response at various incoming intensities for the central pixels
+in a stack of images.
+
+Command line arguments:
+    * `folder`: the folder containing the stacked images to be analysed.
+"""
+
 import numpy as np
 from sys import argv
 from spectacle import io, plot, linearity as lin
 
+# Get the data folder from the command line
 folder = io.path_from_input(argv)
 root, images, stacks, products, results = io.folders(folder)
+
+# Get metadata
 phone = io.load_metadata(root)
-
 colours = io.load_colour(stacks)
+max_value = 2**phone["camera"]["bits"] - 1
 
+# Find the indices of the central pixels
 array_size = np.array(colours.shape)
 mid1, mid2 = array_size // 2
 center = np.s_[mid1:mid1+2, mid2:mid2+2]
 
+# Bayer channels of the central pixels
 colours_here = colours[center].ravel()
 
 # Load the RAW data
@@ -20,19 +33,19 @@ intensities, intensity_errors = intensities_with_errors.T
 means = means.reshape((len(means), -1))
 print("Loaded RAW data")
 
+# Load the JPEG data, if available
 try:
     intensities_with_errors, jmeans = io.load_jmeans(folder, retrieve_value=lin.filename_to_intensity, selection=center)
-    intensities, intensity_errors = intensities_with_errors.T
 except ValueError:
     jpeg = False
 else:
+    intensities, intensity_errors = intensities_with_errors.T
     jpeg = True
     jmeans = jmeans.reshape((len(jmeans), -1, 3))
     print("Loaded JPEG data")
 
+# Normalise the intensities
 intensities = intensities / intensities.max()
-
-max_value = 2**phone["camera"]["bits"]
 
 if jpeg:
     plot.plot_linearity_dng_jpg(intensities, means, jmeans, colours_here, max_value=max_value, savefolder=results/"linearity")
