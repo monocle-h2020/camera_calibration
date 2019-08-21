2ee4b637132916ab48ad78e436afb50967daccf6
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-21 11:04:48 +0200

    Get metadata from an image

---
+++ calibration/generate_metadata.py
@@ -0,0 +1,53 @@
+"""
+Generate metadata files for a camera given a saturated RAW image and user
+inputs on the command line.
+
+Note that if the image does not have at least one saturated pixel, the bit
+depth of the camera (which is important in further calibration steps) cannot
+be determined.
+
+Command line arguments:
+    * `file`: the image to be used.
+
+To do:
+    * Generate entire file structure for camera - or separate script?
+"""
+
+from spectacle import io
+from sys import argv
+
+# Get the data folder from the command line
+file = io.path_from_input(argv)
+
+# Get the data
+raw_file = io.load_raw_file(file)
+bayer_map = io.load_raw_colors(file)
+exif = io.load_exif(file)
+print("Loaded data")
+
+# Get additional data from command line input from the user
+
+# Bit depth - find the maximum value and the corresponding bit depth
+maximum_value = raw_file.raw_image.max()
+bit_depth_conversion = {255: 8, 511: 9, 1023: 10, 2047: 11, 4095: 12, 8191: 13,
+                        16383: 14, 32767: 15, 65535: 16}
+try:
+    bit_depth = bit_depth_conversion[maximum_value]
+except KeyError:
+    raise ValueError(f"The provided image ({file}) does not have any saturated pixels (maximum value: {maximum_value}).")
+
+# Device properties
+device = {
+        "manufacturer": exif["Image Make"].printable,
+        "name": exif["Image Model"].printable
+        }
+print("Device properties:", device)
+
+# Image proprties
+image = {
+        "raw extension": file.suffix,
+        "bias": raw_file.black_level_per_channel,
+        "bayer pattern": raw_file.raw_pattern.tolist(),
+        "bit depth": bit_depth
+        }
+print("Image properties:", image)
