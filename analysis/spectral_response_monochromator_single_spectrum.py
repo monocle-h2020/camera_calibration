df7d1439f364a2e36a980551ef0aa3491bf2e5e8
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-08-27 13:29:27 +0200

    Refactoring

--- analysis/spectral_response_monochromator_single_spectrum.py
+++ analysis/spectral_response_monochromator_single_spectrum.py
@@ -1,43 +1,44 @@
+"""
+Plot a single spectrum generated with a monochromator, e.g. from a single
+grating/filter setting.
+
+This script is intended as a quick check of data quality in case the main
+monochromator processing scripts do not work or produce unexpected results.
+
+Command line arguments:
+    * `folder`: folder containing monochromator data
+"""
+
 import numpy as np
 from sys import argv
-from spectacle import io, raw, plot, calibrate
-from matplotlib import pyplot as plt
+from spectacle import io, spectral
 
-folder, wvl1, wvl2 = io.path_from_input(argv)
-wvl1 = float(wvl1.stem) ; wvl2 = float(wvl2.stem)
+# Get the data folder from the command line
+folder = io.path_from_input(argv)
 root = io.find_root_folder(folder)
+label = folder.stem
+save_folder = root/"analysis/spectral_response/"
+
+# Get the camera metadata
 camera = io.load_metadata(root)
+print("Loaded metadata")
 
-mean_files = sorted(folder.glob("*_mean.npy"))
-stds_files = sorted(folder.glob("*_stds.npy"))
-assert len(mean_files) == len(stds_files)
+# Load the data
+spectrum = spectral.load_monochromator_data(root, folder)
+print("Loaded data")
 
-wvls  = np.zeros((len(mean_files)))
-means = np.zeros((len(mean_files), 4))
-stds  = means.copy()
+# Split the spectrum into its constituents
+wavelengths = spectrum[:,0]
+mean = spectrum[:,1:5]
+stds = spectrum[:,5:]
 
-for j, (mean_file, stds_file) in enumerate(zip(mean_files, stds_files)):
-    m = np.load(mean_file)
-    mean_RGBG, _ = raw.pull_apart(m, camera.bayer_map)
-    sub = mean_RGBG[:,756-50:756+51,1008-50:1008+51]
-    wvls[j] = mean_file.stem.split("_")[0]
-    means[j] = sub.mean(axis=(1,2))
-    stds[j] = sub.std(axis=(1,2))
-    print(wvls[j])
+# Plot the raw spectrum
+spectral.plot_monochromator_curves(wavelengths, [mean], [stds], title=f"{camera.device.name}: Raw spectral curve ({label})", unit="ADU", saveto=save_folder/f"monochromator_{label}_data.pdf")
+print("Saved raw spectrum plot")
 
-means = calibrate.correct_bias(root, means)
+# Calculate the signal-to-noise ratio (SNR) and plot it
+SNR = mean / stds
+SNR_err = np.zeros_like(SNR)  # don't plot errors on the SNR
 
-SNR = means/stds
-
-plt.figure(figsize=(10,5))
-for j, c in enumerate("rgby"):
-    plt.plot(wvls, means[:,j], c=c)
-    plt.fill_between(wvls, means[:,j]-stds[:,j], means[:,j]+stds[:,j], color=c, alpha=0.3)
-plt.xticks(np.arange(0,1000,50))
-plt.xlim(wvl1,wvl2)
-plt.xlabel("Wavelength (nm)")
-plt.ylabel("Spectral response (ADU)")
-plt.ylim(ymin=0)
-plt.savefig(results/f"spectral_response/{folder.stem}.pdf")
-plt.show()
-plt.close()
+spectral.plot_monochromator_curves(wavelengths, [SNR], [SNR_err], title=f"{camera.device.name}: Signal-to-noise ratio ({label})", unit="SNR", saveto=save_folder/f"monochromator_{label}_SNR.pdf")
+print("Saved SNR plot")
