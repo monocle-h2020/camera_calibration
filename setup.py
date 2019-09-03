390817ca203a7cc1814cfc9873e4e4d25c6b9ebb
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-09-03 19:03:23 +0200

    Version 1.1.2 - improved RGBG plotting methods

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
       name="pyspectacle",
-      version="1.1.1",
+      version="1.1.2",
       description="SPECTACLE camera calibration module",
       long_description = long_description,
       url="https://github.com/monocle-h2020/camera_calibration",
