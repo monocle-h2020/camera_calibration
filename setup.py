558e28fbb1f78251960ba19a21a8629982dc617e
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-07-07 14:17:28 +0200

    Version 1.4.0 - CSV calibration data

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
       name="pyspectacle",
-      version="1.3.4",
+      version="1.4.0",
       description="SPECTACLE camera calibration module",
       long_description = long_description,
       url="https://github.com/monocle-h2020/camera_calibration",
