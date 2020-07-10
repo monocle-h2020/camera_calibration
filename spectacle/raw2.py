4eddeac0d63c47f5310793f9bbdbb6d6135edf83
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-07-10 23:27:37 +0200

    Remove duplicates

--- spectacle/raw2.py
+++ spectacle/raw2.py
@@ -1,21 +1,6 @@
 import numpy as np
 
 
-def _find_offset(color_pattern, colour):
-    pos = np.array(np.where(color_pattern == colour)).T[0]
-    return pos
-
-
-def pull_apart(raw_img, color_pattern, color_desc=b"RGBG"):
-    if color_desc != b"RGBG":
-        raise ValueError(f"Image is of type {raw_img.color_desc} instead of RGBG")
-    offsets = np.array([_find_offset(color_pattern, i) for i in range(4)])
-    offX, offY = offsets.T
-    R, G, B, G2 = [raw_img[x::2, y::2] for x, y in zip(offX, offY)]
-    RGBG = np.stack((R, G, B, G2))
-    return RGBG, offsets
-
-
 def pull_apart2(raw_img, color_pattern, color_desc="RGBG", remove=True):
     unique_colours = np.unique(color_pattern)
 
@@ -42,35 +27,3 @@ def pull_apart2(raw_img, color_pattern, color_desc="RGBG", remove=True):
     assert len(np.where(np.nansum(clean_stack, axis=0) != raw_img)[0]) == 0
 
     return clean_stack
-
-
-def put_together(R, G, B, G2, offsets):
-    result = np.zeros((R.shape[0]*2, R.shape[1]*2))
-    for colour, offset in zip([R,G,B,G2], offsets):
-        x, y = offset
-        result[x::2, y::2] = colour
-    result = result.astype(R.dtype)
-    return result
-
-
-def put_together_from_colours(RGBG, colours):
-    original = np.zeros((2*RGBG.shape[1], 2*RGBG.shape[2]))
-    for j in range(4):
-        original[np.where(colours == j)] = RGBG[j].ravel()
-    return original
-
-
-def split_RGBG(RGBG):
-    R, G, B, G2 = RGBG
-    return R, G, B, G2
-
-
-def to_RGB_array(raw_image, color_pattern):
-    RGB = np.zeros((*raw_image.shape, 3))
-    R_ind = np.where(color_pattern == 0)
-    G_ind = np.where((color_pattern == 1) | (color_pattern == 3))
-    B_ind = np.where(color_pattern == 2)
-    RGB[R_ind[0], R_ind[1], 0] = raw_image[R_ind]
-    RGB[G_ind[0], G_ind[1], 1] = raw_image[G_ind]
-    RGB[B_ind[0], B_ind[1], 2] = raw_image[B_ind]
-    return RGB
