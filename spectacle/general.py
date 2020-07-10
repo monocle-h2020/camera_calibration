8afaa45065fcb58a9c171d4ba721d06b5cc5f50a
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-07-10 23:14:42 +0200

    Remove old iSPEX JPEG functions

--- spectacle/general.py
+++ spectacle/general.py
@@ -3,12 +3,6 @@ from scipy.optimize import curve_fit
 from astropy.modeling.blackbody import blackbody_lambda
 import numpy as np
 
-y_thick = (1530, 1911)
-y_thin  = (1970, 2315)
-y = np.concatenate((np.arange(*y_thick), np.arange(*y_thin)))
-x_spectrum = (2150, 3900)
-x = np.arange(*x_spectrum)
-
 
 def gauss_filter(D, sigma=5, **kwargs):
     """
@@ -32,20 +26,6 @@ def gauss_nan(D, sigma=5, **kwargs):
 
     Z=VV/WW
     return Z
-
-
-def split_spectrum(data):
-    thick = data[x_spectrum[0]:x_spectrum[1], y_thick[0]:y_thick[1]]
-    thin  = data[x_spectrum[0]:x_spectrum[1], y_thin[0] :y_thin[1] ]
-    return thick, thin
-
-
-def find_white_balance(data):
-    return data[x_spectrum[0]:x_spectrum[1], :y_thick[0]-100].mean(axis=(0,1))
-
-
-def correct_white_balance(data, white_balance):
-    return data/white_balance
 
 
 def blackbody(wavelengths, temperature=5777, norm=1):
