d00ba99a76e3c8cf7bb2d4ef4e9a0fe7e4440676
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-09-24 14:14:42 +0200

    Plot xyz curves

--- calculate_xyz_matrix.py
+++ calculate_xyz_matrix.py
@@ -8,6 +8,8 @@ Command line arguments:
 """
 
 from sys import argv
+import numpy as np
+from matplotlib import pyplot as plt
 from spectacle import xyz, spectral, io
 
 # Get the data folder from the command line
@@ -17,3 +19,13 @@ root = io.find_root_folder(folder)
 # Load Camera objects
 camera = io.load_camera(root)
 print(f"Loaded Camera object: {camera}")
+
+# Plot xyz curves
+for c, letter in zip([xyz.x, xyz.y, xyz.z], "xyz"):
+    plt.plot(xyz.wavelengths, c, label=f"$\\bar {letter}$")
+plt.xlabel("Wavelength [nm]")
+plt.ylabel("Response")
+plt.legend(loc="best")
+plt.xlim(390, 700)
+plt.ylim(ymin=0)
+plt.show()
