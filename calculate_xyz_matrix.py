6fd903c7cf8bb6ca585421c263b4eb13ac847dc5
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-09-24 14:51:54 +0200

    Plot XYZ, SRF

--- calculate_xyz_matrix.py
+++ calculate_xyz_matrix.py
@@ -20,16 +20,28 @@ root = io.find_root_folder(folder)
 camera = io.load_camera(root)
 print(f"Loaded Camera object: {camera}")
 
-# Plot xyz curves
-for c, letter in zip([xyz.x, xyz.y, xyz.z], "xyz"):
-    plt.plot(xyz.wavelengths, c, label=f"$\\bar {letter}$")
-plt.xlabel("Wavelength [nm]")
-plt.ylabel("Response")
-plt.legend(loc="best")
-plt.xlim(390, 700)
-plt.ylim(ymin=0)
-plt.show()
-
 # Load the SRFs
 camera._load_spectral_response()
 SRF_wavelengths, SRF_RGB = camera.spectral_response[0], camera.spectral_response[1:4]
+
+# Plot xyz curves and SRFs
+kwargs = {"lw": 3}
+colours = ["#d95f02", "#1b9e77", "#7570b3"]
+fig, axs = plt.subplots(nrows=2, figsize=(4,3), sharex=True)
+for c, letter, colour in zip([xyz.x, xyz.y, xyz.z], "xyz", colours):
+    axs[0].plot(xyz.wavelengths, c, c=colour, label=f"$\\bar {letter}$", **kwargs)
+axs[0].set_ylabel("XYZ Response")
+axs[0].legend(loc="upper left", bbox_to_anchor=(1,1))
+axs[0].set_xlim(390, 700)
+axs[0].set_ylim(ymin=0)
+
+for c, letter, colour in zip(SRF_RGB, "rgb", colours):
+    axs[1].plot(SRF_wavelengths, c, c=colour, label=f"${letter}$", **kwargs)
+axs[1].set_xlabel("Wavelength [nm]")
+axs[1].set_ylabel("RGB response")
+axs[1].set_ylim(ymin=0)
+axs[1].legend(loc="upper left", bbox_to_anchor=(1,1))
+
+plt.show()
+plt.close()
+
