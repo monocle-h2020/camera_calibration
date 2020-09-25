7fb23554ceeae1cff4af5383bba5291f89d237d9
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-09-25 13:57:21 +0200

    Remove calculation from analysis script

--- analysis/xyz_matrix_plot.py
+++ analysis/xyz_matrix_plot.py
@@ -10,7 +10,6 @@ Command line arguments:
 """
 
 from sys import argv
-import numpy as np
 from spectacle import spectral, io
 
 # Get the data folder from the command line
@@ -22,7 +21,6 @@ camera = io.load_camera(root)
 print(f"Loaded Camera object: {camera}")
 
 # Save folder
-savefile = camera.filename_calibration("RGB_to_XYZ_matrix.csv")
 savefolder_plot = camera.filename_analysis("spectral_response", makefolders=True)
 
 # Load the SRFs
@@ -32,19 +30,11 @@ SRF_wavelengths, SRF_RGB = camera.spectral_response[0], camera.spectral_response
 # Plot xyz curves and SRFs
 spectral.plot_xyz_and_rgb(SRF_wavelengths, SRF_RGB, label=camera.name, saveto=savefolder_plot/"SRF_vs_XYZ.pdf")
 
-# Calculate the RGB-to-XYZ matrix
-M_RGB_to_XYZ = spectral.calculate_XYZ_matrix(SRF_wavelengths, SRF_RGB)
+# Load the XYZ matrix
+camera._load_XYZ_matrix()
 
 # Determine the base vectors and their chromaticities
-base_xy = spectral.calculate_xy_base_vectors(M_RGB_to_XYZ)
+base_xy = spectral.calculate_xy_base_vectors(camera.XYZ_matrix)
 
 # Plot the colour gamut
 spectral.plot_xy_on_gamut(base_xy, label=camera.name, saveto=savefolder_plot/"colour_space.pdf")
-
-# Save the conversion matrix
-header = f"Matrix for converting {camera.name} RGB data to CIE XYZ, with an equal-energy illuminant (E).\n\
-[X_R  X_G  X_B]\n\
-[Y_R  Y_G  Y_B]\n\
-[Z_R  Z_G  Z_B]"
-np.savetxt(savefile, M_RGB_to_XYZ, header=header, fmt="%1.6f", delimiter=", ")
-print(f"Saved XYZ conversion matrix to `{savefile}`.")
