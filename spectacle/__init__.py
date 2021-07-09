f27ac3a2adc47fc8d017c8218a6f35331b991aae
Olivier Burggraaff <olivierburggraaff@gmail.com>
2021-07-09 17:59:27 +0200

    Remove spectacle.calibrate submodule

--- spectacle/__init__.py
+++ spectacle/__init__.py
@@ -1,4 +1,4 @@
-from . import analyse, calibrate, io
+from . import analyse, io
 from .io import load_raw_file, load_raw_image, load_raw_image_multi, load_exif, load_means, load_stds
 from .general import gauss_filter, weighted_mean, Rsquare, RMS, symmetric_percentiles
 from .camera import Camera, load_camera
