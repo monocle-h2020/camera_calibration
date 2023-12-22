759136e8fe82856e74d400b66d2209295481d36a
Olivier Burggraaff <olivierburggraaff@gmail.com>
2023-12-22 23:13:11 +0100

    Use argparse in generate_camera

--- calibration/generate_camera.py
+++ calibration/generate_camera.py
@@ -5,7 +5,7 @@ Ideally, the given image is one with saturated pixels.
 Note that if the image does not have at least one saturated pixel, the user will be asked to input the bit depth manually.
 
 Command line arguments:
-    * `file`: location of a RAW image from which the data can be extracted.
+    * filename: location of a RAW image from which the data can be extracted.
 
 Example:
     python calibration/generate_camera.py ~/SPECTACLE_data/iPhone_SE/images/IMG_0001.DNG
@@ -13,22 +13,31 @@ Example:
 To do:
     * Generate entire file structure for camera - or separate script?
 """
-from sys import argv
 from spectacle import io
 from spectacle.camera import Camera, name_from_root_folder
 
+# Command-line arguments
+import argparse
+parser = argparse.ArgumentParser(description="Generate a camera metadata file from a RAW image.")
+parser.add_argument("filename", help="Location of RAW image.", type=io.Path)
+parser.add_argument("-o", "--output_folder", help="Folder to save metadata file to (default: input root folder).", type=io.Path, default=None)
+parser.add_argument("-v", "--verbose", help="Enable verbose output.", action="store_true")
+args = parser.parse_args()
+
 # Get the data folder from the command line
-file = io.path_from_input(argv)
-root = io.find_root_folder(file)
+root = io.find_root_folder(args.filename)
 
 # Get the data
-raw_file = io.load_raw_file(file)
-exif = io.load_exif(file)
-print("Loaded data")
+raw_file = io.load_raw_file(args.filename)
+if args.verbose:
+    print("Loaded raw file")
+exif = io.load_exif(args.filename)
+if args.verbose:
+    print("Loaded exif metadata")
 
 # Get the camera name from the root folder, then ask the user for feedback
 camera_name = name_from_root_folder(root)
-camera_name_correct = input(f"From the image location (`{file}`),\nthe camera name was determined to be '{camera_name}'. Is this correct? [y/n]\n")
+camera_name_correct = input(f"From the image location (`{args.filename.absolute()}`),\nthe camera name was determined to be '{camera_name}'. Is this correct? [y/n]\n")
 if camera_name_correct not in ("Y", "y", "yes", "Yes"):
     camera_name = input("Please put in the correct name for this camera:\n")
 
@@ -39,7 +48,7 @@ bit_depth_conversion = {255: 8, 511: 9, 1023: 10, 2047: 11, 4095: 12, 8191: 13,
 try:
     bit_depth = bit_depth_conversion[maximum_value]
 except KeyError:
-    print(f"The provided image ({file}) does not have any saturated pixels (maximum value: {maximum_value}).")
+    print(f"The provided image ({args.filename}) does not have any saturated pixels (maximum value: {maximum_value}).")
     bit_depth = input("Please enter the bit depth manually.\nTypical values are 8 (JPEG), 10 (Samsung), 12 (Apple), 14 (Nikon).\n")
     bit_depth = int(bit_depth)
 
@@ -49,19 +58,22 @@ properties = {
         "manufacturer": exif["Image Make"].printable,
         "name_internal": exif["Image Model"].printable,
         "image_shape": raw_file.raw_image.shape,
-        "raw_extension": file.suffix,
+        "raw_extension": args.filename.suffix,
         "bias": raw_file.black_level_per_channel,
         "bayer_pattern": raw_file.raw_pattern.tolist(),
         "bit_depth": bit_depth,
         "colour_description": raw_file.color_desc.decode()
         }
-print("Camera properties:", properties)
+if args.verbose:
+    print("Camera properties:", properties)
 
 # Combine all metadata into a single object
 camera = Camera(**properties, root=root)
 
 # Save to a filename based on the camera name
-save_to = camera.root/f"{camera.name_underscore}_data.json"
+# Can be overridden by the --output_folder argument
+savefolder = camera.root if args.output_folder is None else args.output_folder
+save_to = savefolder/f"{camera.name_underscore}_data.json"
 
 # Save the Camera object to file
 camera.write_to_file(save_to)
